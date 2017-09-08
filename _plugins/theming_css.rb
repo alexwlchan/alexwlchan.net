f42ea53f400630e8b5b2b29e79154a34b72e9217
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-08 17:44:27 +0100

    Lots of code for themeing

---
+++ _plugins/theming_css.rb
@@ -0,0 +1,19 @@
+Jekyll::Hooks.register :posts, :post_render do |post|
+  if post["theme"] && post["theme"]["color"]
+    color = post["theme"]["color"]
+    mainfile = "theme/style_#{color}.scss"
+    if ! File.file?(mainfile)
+      File.open(mainfile, 'w') { |file| file.write(<<-EOT
+---
+---
+
+$primary-color: ##{color};
+
+@import "_main.scss";
+EOT
+) }
+      puts(mainfile)
+    end
+    puts(post["theme"])
+  end
+end
