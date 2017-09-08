328af0875b3cce03895257a22e87cf53a83432f5
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-08 19:57:05 +0100

    Add a note about building header files

--- _plugins/theming_css.rb
+++ _plugins/theming_css.rb
@@ -1,6 +1,7 @@
 Jekyll::Hooks.register :posts, :post_render do |post|
   if post["theme"] && post["theme"]["color"]
     color = post["theme"]["color"]
+
     mainfile = "theme/style_#{color}.scss"
     if ! File.file?(mainfile)
       File.open(mainfile, 'w') { |file| file.write(<<-EOT
@@ -13,6 +14,11 @@ $primary-color: ##{color};
 EOT
 ) }
       puts(mainfile)
+    end
+
+    headerfile = "theme/specktre_#{color}.png"
+    if ! File.file?(headerfile)
+      puts("we need to build the header file!")
     end
   end
 end
