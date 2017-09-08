3964c80a3ff2b8e4e5c475ce6aec58f6997a34b3
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-08 07:15:56 +0100

    Trim the size of the Docker image

---
+++ install_jekyll.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+# Some links that were useful in getting 'gem install' to work:
+#
+#   - https://jekyllrb.com/docs/installation/
+#   - https://github.com/ffi/ffi/issues/485
+#
+apk update
+apk add build-base libffi-dev ruby ruby-dev ruby-irb ruby-rdoc
+gem install bundler jekyll
+
+# Fixes an issue where bundler complains about running as root.
+# In Docker, that doesn't matter!
+# https://github.com/docker-library/rails/issues/10
+bundle config --global silence_root_warning 1
+
+bundle install
+
+apk del --purge build-base
+rm -rf /var/cache/apk/*
