3ff5ae10396da6571c54d1aebf7b604c2946bbe4
Alex Chan <a.chan@wellcome.ac.uk>
2017-09-08 10:17:49 +0100

    Add tests for year and month archives

--- _tests/run_tests.py
+++ _tests/run_tests.py
@@ -12,3 +12,14 @@ import requests
 def test_pages_appear_correctly(path):
     resp = requests.get(f'http://localhost:5757/{path}')
     assert resp.status_code == 200
+
+
+@pytest.mark.parametrize('path, text_in_page', [
+    ('2017/', 'Posts from 2017'),
+    ('2017/09/', 'Posts from September 2017'),
+    ('', 'Older posts')
+])
+def test_text_appears_in_pages(path, text_in_page):
+    resp = requests.get(f'http://localhost:5757/{path}')
+    assert resp.status_code == 200
+    assert text_in_page in resp.text
