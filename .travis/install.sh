64fc9e1c970c45541956bf759892d616ae06042b
Alex Chan <alex@alexwlchan.net>
2017-09-09 22:30:31 +0100

    Install the feedvalidator library

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,6 +3,10 @@
 set -o errexit
 set -o nounset
 
-pip3 install -r _tests/requirements.txt
+pip install -r _tests/requirements.txt
+
+wget https://github.com/rubys/feedvalidator/archive/master.zip
+unzip master.zip
+pip install -e feedvalidator-master/src
 
 .travis/unfreeze_docker_images.sh
