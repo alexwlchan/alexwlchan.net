b97dd5d97d8294a1241b495c5551f44daacfa528
Alex Chan <alex@alexwlchan.net>
2017-09-09 22:35:54 +0100

    Don't download feedvalidator in the repo

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,9 @@ set -o nounset
 pip install -r _tests/requirements.txt
 
 wget https://github.com/rubys/feedvalidator/archive/master.zip
-unzip master.zip
-pip install -e feedvalidator-master/src
+pushd /tmp
+  unzip master.zip
+  pip install -e feedvalidator-master/src
+popd
 
 .travis/unfreeze_docker_images.sh
