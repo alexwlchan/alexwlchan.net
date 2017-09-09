69e7e9ac3cddf772ed5a914d5b51953fbdaff272
Alex Chan <alex@alexwlchan.net>
2017-09-09 08:19:17 +0100

    Only deploy on push to master

--- .travis/run.sh
+++ .travis/run.sh
@@ -33,6 +33,12 @@ sleep 40
 echo "*** Running the server tests"
 py.test _tests
 
+if [[ "$TRAVIS_EVENT_TYPE" == "pull_request" ]]
+then
+  echo "*** Pull request, skipping deploy to prod"
+  exit 0
+fi
+
 echo "*** Uploading published site to Linode"
 openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in id_rsa_travis.enc -out id_rsa_travis -d
 chmod 400 id_rsa_travis
