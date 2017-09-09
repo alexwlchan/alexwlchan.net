a30c68d0eaeb29fad8d6a07ca6c78b5cc371f650
Alex Chan <alex@alexwlchan.net>
2017-09-09 07:52:56 +0100

    Make the image freezer more verbose

--- .travis/freeze_docker_images.sh
+++ .travis/freeze_docker_images.sh
@@ -2,7 +2,12 @@
 
 set -o errexit
 set -o nounset
+set -o verbose
 
+echo "*** Freezing Docker images"
 mkdir -p _cache
 docker save alexwlchan/alexwlchan.net --output _cache/alexwlchan_build.tar
 docker save alexwlchan/specktre --output _cache/specktre.tar
+
+ls _cache
+set +o verbose
