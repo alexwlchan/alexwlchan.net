b4196e4c5fe99f8ead71f7e9f7e29a76e07588c7
Alex Chan <alex@alexwlchan.net>
2017-09-09 07:46:10 +0100

    Add the Travis key to the repo

--- .travis/run.sh
+++ .travis/run.sh
@@ -38,10 +38,11 @@ docker stop alexwlchan.net_serve
 make publish
 
 echo "*** Uploading published site to Linode"
+openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in id_rsa_travis.enc -out id_rsa_travis -d
 rsync \
   --archive \
   --verbose \
   --compress \
   --delete \
-  --rsh="ssh -o StrictHostKeyChecking=no" \
+  --rsh="ssh -o StrictHostKeyChecking=no -i id_rsa_travis" \
   _site "$RSYNC_USER"@"$RSYNC_HOST":"$RSYNC_DIR"
