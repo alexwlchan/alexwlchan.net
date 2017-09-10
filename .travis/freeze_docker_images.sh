cd24070e14c748364ce487216ab86a4e62944a9f
Alex Chan <alex@alexwlchan.net>
2017-09-10 19:56:46 +0100

    ID, not Id

--- .travis/freeze_docker_images.sh
+++ .travis/freeze_docker_images.sh
@@ -5,7 +5,7 @@ set -o nounset
 
 echo "*** Freezing Docker images"
 mkdir -p /home/travis/docker
-for img in $(docker images --format "{{.Id}}")
+for img in $(docker images --format "{{.ID}}")
 do
   docker save "$img" --output "/home/travis/docker/$img.tar"
 done
