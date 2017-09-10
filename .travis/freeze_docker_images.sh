e66785e849619556b3576ffcf05b0ebf88a939b9
Alex Chan <alex@alexwlchan.net>
2017-09-10 19:46:31 +0100

    Tweak the freezer script

--- .travis/freeze_docker_images.sh
+++ .travis/freeze_docker_images.sh
@@ -5,9 +5,10 @@ set -o nounset
 
 echo "*** Freezing Docker images"
 mkdir -p /home/travis/docker
-for img in $(docker images --format "{{.Repository}} {{.Tag}}" --filter "dangling=false")
+for img in $(docker images --format "{{.Repository}}:{{.Tag}}" --filter "dangling=false")
 do
-  repository=$(echo "$img" | awk '{print $1}')
-  tag=$(echo "$img" | awk '{print $2}')
-  docker save "$repository:$tag" --output /home/travis/docker/${repository/\//_/}--${tag}.tar
+  outfile="${img/\//_}.tar"
+  outfile="${outfile/:/--}"
+  echo "$outfile"
+  docker save "$img" --output "/home/travis/docker/$outfile"
 done
