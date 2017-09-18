b6a3753ab4f3718b47c056b846f805b5ea71c900
Alex Chan <alex@alexwlchan.net>
2017-09-18 21:38:34 +0100

    Install Specktre as part of the main build image

---
+++ install_specktre.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -o errexit
+set -o nounset
+
+apk update
+apk add build-base jpeg-dev optipng python python-dev py2-pip zlib-dev
+
+pip install Pillow
+pip install specktre==0.20.0
+
+# These dependencies are only required to install Pillow, not to actually
+# run it.
+apk del --purge build-base python-dev
+rm -rf /var/cache/apk/*
