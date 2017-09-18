c5f1283096aae7ffe4e94a8cd11bd1ea38992ed8
Alex Chan <alex@alexwlchan.net>
2017-09-18 21:41:18 +0100

    FFS, it's my package, get the version right!

--- install_specktre.sh
+++ install_specktre.sh
@@ -7,7 +7,7 @@ apk update
 apk add build-base jpeg-dev optipng python python-dev py2-pip zlib-dev
 
 pip install Pillow
-pip install specktre==0.20.0
+pip install specktre==0.2.0
 
 # These dependencies are only required to install Pillow, not to actually
 # run it.
