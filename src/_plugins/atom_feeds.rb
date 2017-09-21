b06ec97ba74b74f10a97974dc49856cd9dad29c8
Alex Chan <alex@alexwlchan.net>
2017-09-21 21:00:20 +0100

    Properly minify the XML in the Atom feed

--- src/_plugins/atom_feeds.rb
+++ src/_plugins/atom_feeds.rb
@@ -7,6 +7,19 @@ module Jekyll
       doc.xpath('@style|.//@style|@data-lang|.//@data-lang|@controls|.//@controls').remove
       doc.to_s
     end
+
+    # Matches all whitespace that follows
+    #   1. A '>', which closes an XML tag or
+    #   2. A '}', which closes a Liquid tag
+    # We will strip all of this whitespace to minify the template
+    #
+    # This isn't as nice as using a proper XML parser, but I've been unable
+    # to get that working in Ruby!  This is taken from jekyll-feed.
+    MINIFY_REGEX = %r!(?<=>|})\s+!
+
+    def minify_xml(xml)
+      xml.gsub(MINIFY_REGEX, "")
+    end
   end
 end
 
