95c75234fc1f9418f14e7b743ad06157e45804b2
Alex Chan <alex@alexwlchan.net>
2017-09-21 20:45:23 +0100

    Tidy up a bit of code

--- src/_plugins/atom_feeds.rb
+++ src/_plugins/atom_feeds.rb
@@ -4,11 +4,7 @@ module Jekyll
   module AtomFeedFilters
     def strip_html_attrs(html)
       doc = Nokogiri::HTML.fragment(html)
-
       doc.xpath('@style|.//@style|@data-lang|.//@data-lang|@controls|.//@controls').remove
-      # doc.xpath('@data-lang|.//@data-lang').remove
-      # doc.xpath('@controls|.//@controls').remove
-
       doc.to_s
     end
   end
