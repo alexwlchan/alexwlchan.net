dfc502d8fb273eb92b3b605626441ae7dcef56f1
Alex Chan <alex@alexwlchan.net>
2017-09-21 21:32:06 +0100

    Push the Travis RSA key down into the .travis directory

--- .travis/run.sh
+++ .travis/run.sh
@@ -44,7 +44,7 @@ then
 fi
 
 echo "*** Uploading published site to Linode"
-openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in id_rsa_travis.enc -out id_rsa_travis -d
+openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in .travis/id_rsa.enc -out id_rsa_travis -d
 chmod 400 id_rsa_travis
 export SSHOPTS=" -o StrictHostKeyChecking=no -i id_rsa_travis"
 make deploy
