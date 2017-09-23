6a929b814b8fcac0ed4b83dc3932a3cf44bedc5d
Alex Chan <alex@alexwlchan.net>
2017-09-23 10:17:20 +0100

    Get the tests running inside Docker

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8
+
+import os
+import pytest
+
+
+@pytest.fixture
+def hostname():
+    host = os.environ.get('HOSTNAME', 'localhost')
+    port = os.environ.get('PORT', 5757)
+    return '%s:%s' % (host, port)
