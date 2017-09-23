dc3de7412d0ba80df5fa9d93ae5328846ce1e43a
Alex Chan <alex@alexwlchan.net>
2017-09-23 16:31:13 +0100

    Add a test for broken internal links

---
+++ tests/test_no_broken_links.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8
+"""
+This test checks that all the internal links in the site (links that point to
+other pages on the site) are pointing at working pages.
+"""
+
+from http_crawler import crawl
+
+
+def test_no_links_are_broken():
+    responses = []
+    for rsp in crawl('http://0.0.0.0:5757/', follow_external_links=False):
+        responses.append(rsp)
+
+    failed_responses = [rsp for rsp in responses if rsp.status_code != 200]
+    failures = [f'{rsp.url} ({rsp.status_code})' for rsp in failed_responses]
+    print('\n'.join(failures))
+    assert len(failures) == 0
