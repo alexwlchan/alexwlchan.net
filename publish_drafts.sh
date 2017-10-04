06af2fac2d1a017f7645afd868c0d40e03702229
Alex Chan <alex@alexwlchan.net>
2017-10-04 22:49:33 +0100

    Write a script for publishing new drafts

--- publish_drafts.sh
+++ publish_drafts.sh
@@ -5,3 +5,26 @@
 
 set -o errexit
 set -o nounset
+
+ROOT=$(git rev-parse --show-toplevel)
+SRC=$ROOT/src
+
+if [[ ! -d $SRC/_drafts ]]
+then
+  echo "*** _drafts directory does not exist, so not publishing any drafts"
+  exit 0
+fi
+
+for f in $SRC/_drafts/*.md
+do
+  new_f="$(dirname $f)/$(date +"%Y-%m-%d-")$(basename $f)"
+  mv "$f" "$new_f"
+
+  jekyll publish "$new_f"
+
+  git rm "$f"
+  mv $ROOT/_posts/$(basename "$new_f") $ROOT/_posts/$(date +"%Y")/$(basename "$new_f")
+  git add $ROOT/_posts/$(date +"%Y")/$(basename "$new_f")
+
+  git commit -m "[auto] Publish draft $(basename $f)"
+done
