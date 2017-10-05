2e4d15ec834060c520b1b8c9f3171c84a2bf7c56
Alex Chan <alex@alexwlchan.net>
2017-10-05 20:23:28 +0100

    Give the command the proper name

--- src/_plugins/publish_drafts.rb
+++ src/_plugins/publish_drafts.rb
@@ -3,7 +3,7 @@ module Jekyll
     class Hello < Command
       class << self
         def init_with_program(prog)
-          prog.command(:hello) do |c|
+          prog.command(:"publish-drafts") do |c|
             c.action do |args, options|
               Jekyll.logger.info "Hello!"
             end
