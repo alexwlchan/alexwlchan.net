b412d0a10459c102ee6bb8f55826a5fc6abfb249
Alex Chan <alex@alexwlchan.net>
2017-10-06 19:00:40 +0200

    Call through to jekyll-compose to move the draft

--- src/_plugins/publish_drafts.rb
+++ src/_plugins/publish_drafts.rb
@@ -1,3 +1,5 @@
+require 'jekyll-compose'
+
 # This command does some automation with the _drafts folder.  Specifically,
 # when run, it:
 #
@@ -11,30 +13,65 @@
 # installed as a gem, because installing local gems adds a whole extra layer
 # of complication that I don't want right now.
 
-def publish_all_drafts(drafts_dir: "_drafts")
+def publish_all_drafts(source_dir)
+  drafts_dir = File.join(source_dir, "_drafts")
   if not Dir.exist? drafts_dir
     return
   end
 
+  now = Time.now
+
   Dir.glob("#{drafts_dir}/*.md") do |entry|
-    puts entry
+    puts "*** Publishing draft #{entry}"
+    new_file = Jekyll::Commands::NewPublish.process(
+      args = [entry],
+      options = {'source': source_dir, 'date': now}
+    )
+    puts new_file
   end
 end
 
 module Jekyll
   module Commands
     class PublishDrafts < Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:"publish-drafts") do |c|
+      def self.init_with_program(prog)
+        prog.command(:"publish-drafts") do |c|
 
-            c.option 'source', '-s', '--source SOURCE', 'Custom source directory'
+          c.option 'source', '-s', '--source SOURCE', 'Custom source directory'
 
-            c.action do |args, options|
-              publish_all_drafts()
-            end
+          c.action do |args, options|
+            publish_all_drafts(options['source'])
           end
         end
+      end
+    end
+
+    # Subclasses based on jekyll-compose.  The two relevant changes:
+    #
+    #  * Posts are sorted into per-year directories, because that's the
+    #    folder structure I prefer
+    #  * process() returns the path to the new file, so I can pass it to
+    #    `git add`.
+    #
+
+    class NewPublish < Commands::Publish
+      def self.process(args = [], options = {})
+        params = PublishArgParser.new args, options
+        params.validate!
+
+        movement = NewDraftMovementInfo.new params
+
+        mover = DraftMover.new movement, params.source
+        mover.move
+        movement.to
+      end
+    end
+
+    class NewDraftMovementInfo < Commands::DraftMovementInfo
+      def to
+        date_stamp = params.date.strftime '%Y-%m-%d'
+        year = params.date.strftime "%Y"
+        "_posts/#{year}/#{date_stamp}-#{params.name}"
       end
     end
   end
