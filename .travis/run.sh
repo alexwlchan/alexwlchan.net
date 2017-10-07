d93cf788f43b4e404955dcfdde4ae027a326fdb0
Alex Chan <alex@alexwlchan.net>
2017-10-07 16:04:29 +0200

    we don't want to bail out early

--- .travis/run.sh
+++ .travis/run.sh
@@ -37,11 +37,11 @@ set -o errexit
 echo "*** Running the server tests"
 make test
 
-if [[ "$TRAVIS_EVENT_TYPE" == "pull_request" ]]
-then
-  echo "*** Pull request, skipping deploy to prod"
-  exit 0
-fi
+# if [[ "$TRAVIS_EVENT_TYPE" == "pull_request" ]]
+# then
+#   echo "*** Pull request, skipping deploy to prod"
+#   exit 0
+# fi
 
 echo "*** Uploading published site to Linode"
 openssl aes-256-cbc -K $encrypted_83630750896a_key -iv $encrypted_83630750896a_iv -in .travis/id_rsa.enc -out id_rsa_travis -d
@@ -49,6 +49,7 @@ chmod 400 id_rsa_travis
 export SSHOPTS=" -o StrictHostKeyChecking=no -i id_rsa_travis"
 make publish
 
+echo "*** Pushing any extra commits to GitHub"
 # Set up Git config.
 git config user.name "Travis CI on behalf of Alex Chan"
 git config user.email "travisci_git@alexwlchan.fastmail.co.uk"
