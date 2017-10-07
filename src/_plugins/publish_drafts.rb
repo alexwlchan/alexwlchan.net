ccd4f0cf6183a8f57622eb8110273a8691de5bd2
Alex Chan <alex@alexwlchan.net>
2017-10-07 17:28:08 +0200

    Add the publish_drafts plugin

---
+++ src/_plugins/publish_drafts.rb
@@ -0,0 +1,56 @@
+# This command does some automation with the _drafts folder.  Specifically,
+# when run, it:
+#
+#  * Copies every file from _drafts into _posts, into my preferred folder
+#    structure (one folder per year)
+#  * Creates a Git commit for the change
+#  * Pushes said Git commit to GitHub
+#
+# It gets copied directly into the Docker container, rather than being
+# installed as a gem, because installing local gems adds a whole extra layer
+# of complication that I don't want right now.
+
+require 'shell/executer.rb'
+
+
+def publish_all_drafts()
+  Dir.chdir(source_dir) do
+    drafts_dir = File.join(source_dir, "_drafts")
+    if not Dir.exist? drafts_dir
+      return
+    end
+
+    now = Time.now
+
+    Dir.glob("#{drafts_dir}/*.md") do |entry|
+      puts "*** Publishing draft #{entry}"
+
+      now = Time.now
+
+      name = File.basename(entry)
+      new_name = "_posts/#{now.strftime('%Y')}/#{now.strftime('%Y-%m-%d')}-#{name}"
+      File.rename(name, new_name)
+
+      puts "*** Creating Git commit for #{entry}"
+      Shell.execute!("git rm #{entry}")
+      Shell.execute!("git add #{new_name}")
+      Shell.execute!("git commit -m \"[auto] Publish draft entry #{entry}\"")
+    end
+  end
+end
+
+
+module Jekyll
+  module Commands
+    class PublishDrafts < Command
+      def self.init_with_program(prog)
+        prog.command(:"publish-drafts") do |c|
+
+          c.action do |args, options|
+            publish_all_drafts()
+          end
+        end
+      end
+    end
+  end
+end
