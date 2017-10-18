c28b0f5ea02652f7899e34d9a8779e2ff47d8020
Alex Chan <alex@alexwlchan.net>
2017-10-18 07:33:41 +0100

    Move the responses to a cacheable fixture

--- tests/test_internal_links.py
+++ tests/test_internal_links.py
@@ -1,16 +1,23 @@
 # -*- encoding: utf-8
 
 from http_crawler import crawl
+import pytest
+
+_responses = []
 
 
-def test_no_links_are_broken(baseurl):
+@pytest.fixture
+def responses(baseurl):
+    if not _responses:
+        for rsp in crawl(baseurl, follow_external_links=False):
+            _responses.append(rsp)
+    return _responses
+
+
+def test_no_links_are_broken(responses):
     """
     Check that all internal links point to working pages.
     """
-    responses = []
-    for rsp in crawl(baseurl, follow_external_links=False):
-        responses.append(rsp)
-
     failed_responses = [rsp for rsp in responses if rsp.status_code != 200]
     failures = [
         '%s (%d)' % (rsp.url, rsp.status_code)
