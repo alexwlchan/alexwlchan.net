285343a18028830d6d7d9413a5a533c5f6961e0a
Alex Chan <alex@alexwlchan.net>
2017-10-18 07:49:50 +0100

    Add a test that every image is linked somewhere

--- tests/test_internal_links.py
+++ tests/test_internal_links.py
@@ -1,5 +1,8 @@
 # -*- encoding: utf-8
 
+import os
+from urlparse import urlparse
+
 from http_crawler import crawl
 import pytest
 
@@ -8,6 +11,10 @@ _responses = []
 
 @pytest.fixture
 def responses(baseurl):
+    """
+    Uses http-crawler to index every internal page, and return a list
+    of associated responses.
+    """
     if not _responses:
         for rsp in crawl(baseurl, follow_external_links=False):
             _responses.append(rsp)
@@ -25,3 +32,32 @@ def test_no_links_are_broken(responses):
     ]
     print('\n'.join(failures))
     assert len(failures) == 0
+
+
+def images_in_repo(repo):
+    """
+    Generates paths to all the image files in the ``src`` directory.
+    """
+    images_dir = os.path.join(repo, 'src', '_images')
+    for root, _, filenames in os.walk(images_dir):
+        for f in filenames:
+            if f.startswith('.'):
+                continue
+            yield os.path.join(root, f)
+
+
+def test_all_images_are_linked_somewhere(repo, responses):
+    """
+    Check that every image in ``src/_images`` is linked somewhere on the site.
+    """
+    paths = [urlparse(rsp.url).path for rsp in responses]
+    img_paths = {p for p in paths if p.startswith('/images/')}
+
+    local_paths = set()
+    for path in images_in_repo(repo):
+        local_path = path.replace(repo, '').replace('src/_images', 'images')
+        local_paths.add(local_path)
+
+    unlinked_paths = local_paths - img_paths
+    print('\n'.join(unlinked_paths))
+    assert len(unlinked_paths) == 0
