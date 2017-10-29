857650bfc26ba8cce85f40047b76d6d528a79f04
Alex Chan <alex@alexwlchan.net>
2017-10-29 23:49:55 +0000

    Get the RSA key in the right place

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -12,7 +12,8 @@ openssl aes-256-cbc \
 
 echo "*** Uploading published site to Linode"
 chmod 400 id_rsa
-export SSHOPTS=" -o StrictHostKeyChecking=no -i id_rsa"
+mv id_rsa ~/.ssh/id_rsa
+export SSHOPTS=" -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa"
 make deploy
 
 echo "*** Configuring Git"
