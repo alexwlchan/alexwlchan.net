74401c6bd56d154059fc2b7bc08d8da4a3bdb66a
Alex Chan <alex@alexwlchan.net>
2017-11-01 11:19:56 +0000

    Only record each broken link once

--- tests/test_internal_links.py
+++ tests/test_internal_links.py
@@ -37,10 +37,10 @@ def test_no_links_are_broken(responses):
     Check that all internal links point to working pages.
     """
     failed_responses = [rsp for rsp in responses if rsp.status_code != 200]
-    failures = [
+    failures = set([
         '%s (%d)' % (rsp.url, rsp.status_code)
         for rsp in failed_responses
-    ]
+    ])
     print('\n'.join(sorted(failures)))
     assert len(failures) == 0
 
