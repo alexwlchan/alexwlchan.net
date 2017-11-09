ac0431268fae54af1c1a99bf04e8618b0df71e3e
Alex Chan <alex@alexwlchan.net>
2017-11-09 21:13:21 +0000

    Skip installing specktre in CI

--- install_specktre.sh
+++ install_specktre.sh
@@ -3,6 +3,14 @@
 set -o errexit
 set -o nounset
 
+# Any specktre assets are compiled locally and checked in before they're
+# sent to CI, so CI never needs to install these pieces.
+if [[ "${CI:-false}" == "true" ]]
+then
+    echo "*** Running in CI, so skipping installing specktre"
+    exit 0
+fi
+
 apk update
 apk add python py2-pillow py2-pip
 
