6af27e2618029df5fe8325e2ef51e897382cb656
Alex Chan <alex@alexwlchan.net>
2017-11-09 09:34:05 +0000

    Fix the existing lint errors

--- tests/test_links.py
+++ tests/test_links.py
@@ -26,9 +26,11 @@ def test_pages_appear_correctly(hostname, path):
     ('2017/09/ode-to-docopt', u'I’ve used it in multiple languages'),
 
     # Syntax highlighting is being applied correctly
-    ('2017/09/useful-git-commands/', '''<code class="language-console" data-lang="console"><span></span><span class="gp">$</span> git rev-parse --show-toplevel
-<span class="go">/Users/alexwlchan/repos/alexwlchan.net</span>
-</code>'''),
+    ('2017/09/useful-git-commands/',
+     '<code class="language-console" data-lang="console"><span></span>'
+     '<span class="gp">$</span> git rev-parse --show-toplevel\n'
+     '<span class="go">/Users/alexwlchan/repos/alexwlchan.net</span>\n'
+     '</code>'),
 
     # We're not adding trailing commas to tags
     ('2017/09/ode-to-docopt', 'python</a>, <a'),
