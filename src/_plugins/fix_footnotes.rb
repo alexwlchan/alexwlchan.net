73f0561ae6ef2eb925312396d57053cec0cbb2a6
Alex Chan <alex@alexwlchan.net>
2017-11-20 23:02:06 +0000

    Fix footnote markers in HTML pages

---
+++ src/_plugins/fix_footnotes.rb
@@ -0,0 +1,13 @@
+# Make sure that footnote markers are rendered as a text
+# arrow on iOS devices, not emoji.  For more info:
+# http://daringfireball.net/linked/2015/04/22/unicode-emoji
+
+module Jekyll
+  module FootnoteFilter
+    def fix_footnote(input)
+      input.gsub("&#8617;", "&#8618;&#xFE0E;")
+    end
+  end
+end
+
+Liquid::Template::register_filter(Jekyll::FootnoteFilter)
