7ea09c052110a49b37a84ef9586d53a236f33126
Alex Chan <alex@alexwlchan.net>
2017-11-28 20:55:57 +0000

    Fix the analytics script to run properly

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -2,7 +2,7 @@
   if (navigator.doNotTrack == 1) {
     console.log("Do Not Track is enabled; don't run analytics 🙈");
   } else {
-    var doc = document; enc = encodeURIComponent; img = new Image;
-    img.src = "%s/a.gif?url=" + enc(doc.location.href) + "&ref=" + enc(doc.referrer) + "&t=" enc(doc.title);
+    var doc = document, enc = encodeURIComponent, img = new Image;
+    img.src = "%s/a.gif?url=" + enc(doc.location.href) + "&ref=" + enc(doc.referrer) + "&t=" + enc(doc.title);
   }
 })()
