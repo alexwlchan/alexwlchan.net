87a1bd5a664c9f9f227e1fdf52f451153249995b
Alex Chan <alex@alexwlchan.net>
2017-11-28 20:42:00 +0000

    Don't make an analytics call if Do Not Track is set

--- analytics/analytics.js
+++ analytics/analytics.js
@@ -1,4 +1,8 @@
-(function(){
-    var d=document,i=new Image,e=encodeURIComponent;
-    i.src='%s/a.gif?url='+e(d.location.href)+'&ref='+e(d.referrer)+'&t='+e(d.title);
-    })()
+(function() {
+  if (navigator.doNotTrack == 1) {
+    console.log("Do Not Track is enabled; don't run analytics 🙈");
+  } else {
+    var doc = document; enc = encodeURIComponent; img = new Image;
+    img.src = "%s/a.gif?url=" + enc(doc.location.href) + "&ref=" + enc(doc.referrer) + "&t=" enc(doc.title);
+  }
+})()
