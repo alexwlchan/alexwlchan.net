0f6247e3cddbd3e37378dc9c5855aa4598a63d2d
Alex Chan <alex@alexwlchan.net>
2017-12-09 20:33:19 +0000

    Handle empty referrers correctly

--- analytics/test_analytics.py
+++ analytics/test_analytics.py
@@ -2,7 +2,7 @@
 
 import pytest
 
-from reports import NGINX_LOG_REGEX
+import reports
 
 
 @pytest.mark.parametrize('log_line', [
@@ -14,4 +14,27 @@ from reports import NGINX_LOG_REGEX
     '0.0.0.0 - - [01/Jan/2001:00:00:00 +0000] "GET /example HTTP/1.0" 200 0 "" "Example user agent" "1.2.3.4"',
 ])
 def test_nginx_regex(log_line):
-    assert NGINX_LOG_REGEX.match(log_line) is not None
+    assert reports.NGINX_LOG_REGEX.match(log_line) is not None
+
+
+log_line_params = {
+    'forwarded_host': '1.2.3.4',
+    'datetime': '01/Jan/2001:00:00:00 +0000',
+    'method': 'GET',
+    'url': '/',
+    'status': '200',
+    'bytes_sent': '0',
+    'referrer': 'http://referrer.org',
+    'user_agent': 'WebKit',
+}
+
+
+@pytest.mark.parametrize('override_params, expected_result', [
+    ({'referrer': 'http://yandex.ru'}, True),
+    ({'referrer': '-'}, False),
+])
+def test_should_be_rejected(override_params, expected_result):
+    params = log_line_params.copy()
+    params.update(override_params)
+    line = reports.LogLine(**params)
+    assert reports.should_be_rejected(line) is expected_result
