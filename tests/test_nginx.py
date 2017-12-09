410595c86e26956b77f2e69178fe6bd8ca342f7c
Alex Chan <alex@alexwlchan.net>
2017-12-09 22:05:14 +0000

    Put a file in place for /atom.xml

--- tests/test_nginx.py
+++ tests/test_nginx.py
@@ -11,3 +11,13 @@ import pytest
 def test_nginx_resolves_correctly(nginx_hostname, path, expected_status):
     resp = requests.get('http://%s%s' % (nginx_hostname, path))
     assert resp.status_code == expected_status
+
+
+@pytest.mark.parametrize('path, expected_location', [
+    ('/feeds/all.atom.xml', '/atom.xml'),
+])
+def test_nginx_resolves_correctly(nginx_hostname, path, expected_location):
+    resp = requests.head('http://%s%s' % (nginx_hostname, path))
+    assert resp.status_code == 301
+    expected_location = 'https://alexwlchan.net' + expected_location
+    assert resp.headers['Location'] == expected_location
