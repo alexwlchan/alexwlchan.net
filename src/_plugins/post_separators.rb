41b49d13aef62ddbfe32df1d6e0800e7dfb32433
Alex Chan <alex@alexwlchan.net>
2018-03-11 19:52:37 +0000

    Revert "home.html: move post separators into a plugin"
    197afd5dbf730c6bc9d8c7f2046ac235a24172f9

--- src/_plugins/post_separators.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  class PostSeparatorTag < Liquid::Tag
-    def render(context)
-      "<div class=\"post__separator\" aria-hidden=\"true\">&#9671;</div>"
-    end
-  end
-end
-
-Liquid::Template.register_tag('post_separator', Jekyll::PostSeparatorTag)
