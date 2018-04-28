5d96b8576dae600e7cdc55f53a93f67313d3ac80
Alex Chan <alex@alexwlchan.net>
2018-04-28 06:49:46 +0000

    Start moving the rejections logic into a separate file

---
+++ analytics/rejections.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8
+"""
+I get a bunch of requests that are uninteresting for some reason -- maybe
+somebody trying to find a PHP admin page, or crawling for vulnerable WordPress
+instances.  Any such request can immediately be rejected as uninteresting
+for my analytics.
+"""
+
+from urllib.parse import urlparse
+
+
+BAD_PATHS = [
+    '/admin/',
+    '/dbadmin/',
+]
+
+
+def should_be_rejected(log_line):
+    if urlparse(log_line.url).path in BAD_PATHS:
+        return True
+
+    return False
