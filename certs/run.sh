d54a68741a4fb18d05e04c606132120f69574939
Alex Chan <alex@alexwlchan.net>
2018-06-25 14:50:57 +0000

    Fix all the cert issues

--- certs/run.sh
+++ certs/run.sh
@@ -5,21 +5,14 @@ set -o nounset
 
 DOMAIN="$1"
 
-chmod 600 cloudflare_credentials
+chmod 600 "$CERTS"/cloudflare_credentials
 
 docker run \
-  # https://certbot.eff.org/docs/using.html#id6 ("Lock Files")
   --volume ~/letsencrypt/config:/etc/letsencrypt \
   --volume ~/letsencrypt/work:/var/lib/letsencrypt \
   --volume ~/letsencrypt/logs:/var/log/letsencrypt \
-
-  # Provides CloudFlare credentials
-  -v $(pwd):/certs alexwlchan/certbot \
-
-  certonly --domain "*.$DOMAIN" \
-
-    # Required if you're requesting wildcard certs
+  --volume "$CERTS":/certs alexwlchan/certbot \
+  certonly --domain '*.'$DOMAIN \
     --server https://acme-v02.api.letsencrypt.org/directory \
-
     --dns-cloudflare --dns-cloudflare-credentials=/certs/cloudflare_credentials \
     --non-interactive --agree-tos --email=certbot@alexwlchan.fastmail.co.uk
