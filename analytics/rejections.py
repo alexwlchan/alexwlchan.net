17369015060e3db924859960d4f1a539e7e9c1bd
Alex Chan <alex@alexwlchan.net>
2018-08-20 08:16:35 +0100

    Move some of the analytics config into a TOML file

--- analytics/rejections.py
+++ analytics/rejections.py
@@ -8,163 +8,33 @@ for my analytics.
 
 from urllib.parse import urlparse
 
+import toml
 
-BAD_PATHS = [
-    '/admin/',
-    '/dbadmin/',
-    '/myadmin/',
-    '/mysql/',
-    '/mysqladmin/',
-    '/mysql-admin/',
-    '/mysqlmanager/',
-    '/sqlmanager/',
-    '/sqlweb/',
-    '/webadmin/',
-    '/webdb/',
-    '/websql/',
 
-    '/p/m/a/',
-    '/pma/',
-    '/pma2005/',
-    '/phpmyadmin/',
-    '/phpmyadmin2/',
-    '/php-my-admin/',
-    '/php-myadmin/',
-    '/phpmy-admin/',
-
-    '/hnap1/',
-
-    '/hudson/script',
-    '/script',
-
-    '/https:/alexwlchan.net/',
-
-    '/.git/HEAD',
-
-    '/themes/default/js/showdown.js',
-
-    '/filezilla.xml',
-    '/sitemanager.xml',
-    '/winscp.ini',
-    '/ws_ftp.ini',
-
-    '/simpla/',
-    '/umbraco',
-
-    '/admin/content/sitetree/',
-    '/manager/',
-
-    '/core/themes/bartik/color/preview.html',
-
-    '/id_dsa',
-    '/id_rsa',
-    '/.ssh/id_rsa',
-    '/.ssh/id_dsa',
-    '/.ssh/id_ecdsa',
-    '/.ssh/id_ed25519',
-
-    # These paths don't resolve, nor is there any sensible reason to
-    # expect they might resolving soon.
-    '/2/favicon.ico',
-    '/3/favicon.ico',
-    '/home/favicon.ico',
-    '/.ftpconfig',
-    '/.remote-sync.json',
-    '/.vscode/ftp-sync.json',
-    '/deployment-config.json',
-    '/sftp-config.json',
-]
-
-BAD_PATH_PREFIXES = [
-    '/phpmyadmin-',
-
-    '/wp-admin',
-    '/wp-content',
-    '/wp-includes',
-
-    '/.git',
-]
-
-BAD_PATH_SUFFIXES = [
-    '/uploadify/uploadify.css',
-    '/fix-ie.css',
-
-    '/phpmyadmin/',
-    '/phpmyadmin',
-
-    '/cookies.js',
-
-    '/joomla.xml',
-
-    '/backgroundStripe-new.jpg',
-    '/footerBackground.jpg',
-    '/spacer.png',
-
-    '/ArticleBookmark@2x.png',
-
-    '/null',
-
-    # File formats I don't have anywhere on the site, because I don't
-    # use these technologies!
-    '.asp',
-    '.aspx',
-    '.php',
-]
-
-# Anything on this list was polluting 404 reports with URLs that
-# wouldn't be expected to work -- but it's not a human, so I don't
-# care that it got an error.
-BAD_USER_AGENTS = [
-    'http://ahrefs.com/robot/',
-    'http://megaindex.com/crawler',
-    'http://mj12bot.com/',
-    'http://napoveda.seznam.cz/en/seznambot-intro/',
-    'http://www.baidu.com/search/spider.html',
-    'http://www.similartech.com/smtbot',
-    'DatabaseDriverMysqli',
-    'Googlebot',
-    'Newsify Feed Fetcher',
-    'OpenLinkProfiler.org/bot',
-    'python-requests',
-]
-
-BAD_REFERRERS = [
-    'http://test-iphone-x.com/',
-    'https://yellowstonevisitortours.com',
-    'https://www.cloudsendchef.com',
-    'https://www.timer4web.com/',
-    'https://www.theautoprofit.ml',
-]
-
-BAD_REFERRER_COMPONENTS = [
-    'blog1989.com',
-    'incomekey.net',
-    'yandex.ru',
-    '/www.blog100.org/',
-]
+config = toml.loads(open('rejections.toml').read())
 
 
 def should_be_rejected(log_line):
     parts = urlparse(log_line.url.lower())
 
-    if parts.path in BAD_PATHS:
+    if parts.path in config['bad_paths']:
         return True
 
-    if parts.path.startswith(tuple(BAD_PATH_PREFIXES)):
+    if parts.path.startswith(tuple(config['bad_path_prefixes'])):
         return True
 
-    if parts.path.endswith(tuple([s.lower() for s in BAD_PATH_SUFFIXES])):
+    if parts.path.endswith(tuple([s.lower() for s in config['bad_path_suffixes']])):
         return True
 
-    if any(u in log_line.user_agent for u in BAD_USER_AGENTS):
+    if any(u in log_line.user_agent for u in config['bad_user_agents']):
         return True
 
-    if log_line.referrer in BAD_REFERRERS:
+    if log_line.referrer in config['bad_referrers']:
         return True
 
     if (
         log_line.referrer is not None and
-        any(r in log_line.referrer.lower() for r in BAD_REFERRER_COMPONENTS)
+        any(r in log_line.referrer.lower() for r in config['bad_referrer_components'])
     ):
         return True
 
