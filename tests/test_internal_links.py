bfa30cf4a2c0b24ea0b3a810baae95f62f3b29dc
Alex Chan <a.chan@wellcome.ac.uk>
2018-09-22 17:59:09 +0100

    Screw it, I'm deleting this entire test as not-very-useful

--- tests/test_internal_links.py
+++ tests/test_internal_links.py
@@ -66,32 +66,3 @@ def test_no_links_are_broken(responses):
     ])
     print('\n'.join(sorted(failures)))
     assert len(failures) == 0
-
-
-def images_in_repo(src):
-    """
-    Generates paths to all the image files in the ``src`` directory.
-    """
-    images_dir = os.path.join(src, '_images')
-    for root, _, filenames in os.walk(images_dir):
-        for f in filenames:
-            if f.startswith('.'):
-                continue
-            yield os.path.join(root, f)
-
-
-def test_all_images_are_linked_somewhere(src, responses):
-    """
-    Check that every image in ``src/_images`` is linked somewhere on the site.
-    """
-    paths = [urlparse(rsp.url).path for rsp in responses]
-    img_paths = {p for p in paths if p.startswith('/images/')}
-
-    local_paths = set()
-    for path in images_in_repo(src):
-        local_path = path.replace(src, '').replace('_images', 'images')
-        local_paths.add(local_path)
-
-    unlinked_paths = local_paths - img_paths
-    print('\n'.join(sorted(unlinked_paths)))
-    assert len(unlinked_paths) == 0
