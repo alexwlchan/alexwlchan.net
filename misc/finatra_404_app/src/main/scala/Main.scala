6cffa052fcef94a52d68c13f31c4beff1cfe67b9
Alex Chan <alex@alexwlchan.net>
2018-10-22 19:10:59 +0100

    Get the rest of the Finatra app checked in

--- misc/finatra_404_app/src/main/scala/Main.scala
+++ misc/finatra_404_app/src/main/scala/Main.scala
@@ -1,3 +1,27 @@
-object Main extends App {
-  println("Hello, World!")
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finatra.http.{Controller, HttpServer}
+import com.twitter.finatra.http.routing.HttpRouter
+
+
+class CustomController() extends Controller {
+  get("/healthcheck") { request: Request =>
+    response.ok.json(Map("status" -> "ok"))
+  }
+
+  get("/:*") { request: Request =>
+    response.notFound.json(Map(
+      "status" -> 404,
+      "description" -> s"Page not found for URL ${request.uri}"
+    ))
+  }
 }
+
+
+class Server extends HttpServer {
+  override def configureHttp(router: HttpRouter) {
+    router.add[CustomController]
+  }
+}
+
+
+object ServerMain extends Server
