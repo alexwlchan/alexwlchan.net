1c67e1d3c9f5333df1e45a2b40b0743ec8f96d70
Alex Chan <alex@alexwlchan.net>
2018-11-07 08:39:13 +0000

    One more paragraph and image, plus script improvements

--- resize_image.py
+++ resize_image.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8
 
+import datetime as dt
 import itertools
 import os
+import subprocess
 import sys
 
 from PIL import Image
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+ROOT = subprocess.check_output(
+    ["git", "rev-parse", "--show-toplevel"], cwd=SCRIPT_DIR).strip().decode("utf8")
 
 
 if __name__ == '__main__':
@@ -22,9 +30,17 @@ if __name__ == '__main__':
 
         width = i * 950
         height = int(float(width) / im.width * im.height)
-        print((width, height))
 
         im2 = im.resize((width, height))
-        im2.save(
-            os.path.splitext(path)[0] + "_%dx.jpg" % i
+        image_dir = os.path.join(
+            SCRIPT_DIR, "src", "_images", dt.datetime.now().strftime("%Y")
         )
+        os.makedirs(image_dir, exist_ok=True)
+        out_path = os.path.join(
+            image_dir, os.path.basename(os.path.splitext(path)[0]) + "_%dx.jpg" % i
+        )
+        im2.save(out_path)
+        subprocess.check_call(["git", "add", out_path], cwd=ROOT)
+
+    print(os.path.basename(os.path.splitext(path)[0]))
+    print("Up to %dx" % (i - 1))
