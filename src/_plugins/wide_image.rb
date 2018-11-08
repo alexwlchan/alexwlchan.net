0b612158a67234915c07e3bd1649d5394e825cd1
Alex Chan <alex@alexwlchan.net>
2018-11-08 10:14:55 +0000

    Hopefully final draft of the Aberdulais post

--- src/_plugins/wide_image.rb
+++ src/_plugins/wide_image.rb
@@ -14,10 +14,16 @@ module Jekyll
   <img src="/images/#{@image_id}_1x.jpg" srcset="/images/#{@image_id}_1x.jpg 1x, /images/#{@image_id}_2x.jpg 2x, /images/#{@image_id}_3x.jpg 3x, /images/#{@image_id}_4x.jpg 4x">
 </a>
 EOT
-      else
+      elsif File.exists? "#{site.config["source"]}/_images/#{@image_id}_3x.jpg"
         <<-EOT
 <a href="/images/#{@image_id}_3x.jpg">
   <img src="/images/#{@image_id}_1x.jpg" srcset="/images/#{@image_id}_1x.jpg 1x, /images/#{@image_id}_2x.jpg 2x, /images/#{@image_id}_3x.jpg 3x">
+</a>
+EOT
+      else
+        <<-EOT
+<a href="/images/#{@image_id}_2x.jpg">
+  <img src="/images/#{@image_id}_1x.jpg" srcset="/images/#{@image_id}_1x.jpg 1x, /images/#{@image_id}_2x.jpg 2x">
 </a>
 EOT
       end
