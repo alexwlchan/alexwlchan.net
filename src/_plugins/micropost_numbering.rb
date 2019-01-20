796192b0fb10ba7ca819577eaba37e27d2666e93
Alex Chan <alex@alexwlchan.net>
2019-01-20 23:56:25 +0000

    Start breaking the assumption that every post needs a title

--- src/_plugins/micropost_numbering.rb
+++ src/_plugins/micropost_numbering.rb
@@ -4,6 +4,13 @@ Jekyll::Hooks.register :site, :post_read do |site|
     .sort_by! { |micropost| micropost.date }
     .each
     .with_index(1) { |micropost, mp_number|
+
+      # Was this micropost title created automatically?  If so, let's ditch
+      # it -- we don't actually want it.
+      if micropost.data["title"] == Jekyll::Utils.titleize_slug(micropost.data["slug"])
+        micropost.data.delete("title")
+      end
+
       micropost.data["is_micropost"] = true
       micropost.data["micropost_number"] = mp_number
     }
