fce47551dd52a106d5b6a26f83583f08e79e09e1
Alex Chan <alex@alexwlchan.net>
2019-02-26 18:56:58 +0000

    For now remove all the checks in Travis

--- src/_plugins/html_proof.rb
+++ src/_plugins/html_proof.rb
@@ -1,5 +1,11 @@
 require "html-proofer"
 
 Jekyll::Hooks.register :site, :post_write do |site|
-  HTMLProofer.check_directory(site.config["destination"]).run
+#   HTMLProofer.check_directory(
+#     site.config["destination"], opts = {
+#       :check_html => false,
+#       :check_img_http => true,
+#       :disable_external => true,
+#       :report_invalid_tags => true,
+#     }).run
 end
