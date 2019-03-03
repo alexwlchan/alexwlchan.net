0f23b73a2ee850b9faf77df15bd0630c45e6d27a
Alex Chan <alex@alexwlchan.net>
2019-03-03 14:38:03 +0000

    Add a Jekyll tag for rendering images

--- src/_plugins/images.rb
+++ src/_plugins/images.rb
@@ -1,5 +1,24 @@
-def render_image(path:, alt_text:, title:)
+def render_image(src:, alt_text:, title:)
   <<-EOT
-<a href="#{path}"><img src="#{path}" alt="#{alt_text}" title="#{title}"></a>
+<a href="#{src}"><img src="#{src}" alt="#{alt_text}" title="#{title}"></a>
 EOT
 end
+
+
+module Jekyll
+  class PostImageTag < Alexwlchan::Tag
+    def bind_params(params)
+      @filename = params[:filename] or raise SyntaxError, "Error in tag 'image', :filename parameter is required"
+      @alt_text = params[:alt] or raise SyntaxError, "Error in tag 'image', :alt_text parameter is required"
+      @title = params.fetch(:title, @alt_text)
+    end
+
+    def internal_render
+      src = "/images/#{@context.registers[:page]["date"].year}/#{@filename}"
+      render_image(src: src, alt_text: @alt_text, title: @title)
+    end
+  end
+end
+
+
+Liquid::Template.register_tag("image", Jekyll::PostImageTag)
