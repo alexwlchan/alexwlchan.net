112a67b91fe8e5c32b5173bfe9e517bcb41d1c08
Alex Chan <alex@alexwlchan.net>
2019-03-10 22:18:18 +0000

    Skip the auto_merge_branch script if the PR is incomplete

--- .github/auto_merge_branch/merge_branch.py
+++ .github/auto_merge_branch/merge_branch.py
@@ -37,6 +37,22 @@ if __name__ == '__main__':
     github_event_path = os.environ["GITHUB_EVENT_PATH"]
     event_data = json.load(open(github_event_path))
 
+    check_run = event_data["check_run"]
+    name = check_run["name"]
+
+    # We should only merge pull requests that have the conclusion "succeeded".
+    #
+    # We get a check_run event in GitHub Actions when the underlying run is
+    # scheduled and completed -- if it doesn't have a conclusion, this field is
+    # set to "null".  In that case, we give up -- we'll get a second event when
+    # the run completes.
+    #
+    # See https://developer.github.com/v3/activity/events/types/#CheckRunEvent
+    #
+    if check_run["conclusion"] is None:
+        print(f"*** Check run {name} has not completed, skipping")
+        sys.exit(0)
+
     sess = get_session(github_token)
 
     # Validate the GitHub token
