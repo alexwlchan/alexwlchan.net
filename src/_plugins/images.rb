93a1e2c41f2a2bd70dc7fae0b8dd26090790bb54
Alex Chan <alex@alexwlchan.net>
2019-03-14 00:04:46 +0000

    Locate the matching image file from the _images dir

--- src/_plugins/images.rb
+++ src/_plugins/images.rb
@@ -19,12 +19,39 @@ module Jekyll
       params[:alt] or raise SyntaxError, "Error in tag 'image', :alt parameter is required"
     end
 
+    def find_matching_images(year, filename)
+      base, ext = filename.split(".")
+      source = @context.registers[:site].config["source"]
+
+      # Find all the images that have the same name or have a _1x, _2x, _3x etc.
+      # suffix that suggest different sizes.
+      Dir["#{source}/_images/#{year}/#{base}*"]
+        .map { |path| path.split("/")[-1] }
+        .select { |f|
+          f_base, _ = f.split(".")
+          f_base == base || f_base =~ /_\dx$/
+        }
+    end
+
+    def build_url(filename)
+      "/images/#{@context.registers[:page]["date"].year}/#{filename.split("/")[-1]}"
+    end
+
     def internal_render
       attrs = @params
       attrs[:title] = @params.fetch(:title, @alt_text)
-
       filename = attrs.delete(:filename)
-      attrs[:src] = "/images/#{@context.registers[:page]["date"].year}/#{filename}"
+
+      files = find_matching_images(@context.registers[:page]["date"].year, filename)
+      if files.size == 0
+        raise SyntaxError, "Cannot find file for #{filename}"
+      elsif files.size == 1
+        attrs[:src] = build_url(files[0])
+      else
+        puts files
+        raise SyntaxError, "Too many files for #{filename}"
+      end
+
       render_image(**attrs)
     end
   end
