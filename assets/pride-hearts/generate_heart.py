0650ddaa26e7bcb268b45259043ac473c05b3a28
Alex Chan <alex@alexwlchan.net>
2019-06-17 12:26:23 +0100

    Start breaking up the coordinate generators

--- assets/pride-hearts/generate_heart.py
+++ assets/pride-hearts/generate_heart.py
@@ -1,83 +1,26 @@
-heart10 = """
- XX    XX
-XXXX  XXXX
-XXXXXXXXXX
-XXXXXXXXXX
-XXXXXXXXXX
-XXXXXXXXXX
- XXXXXXXX
-  XXXXXX
-   XXXX
-    XX
-"""
+#!/usr/bin/env python
+# -*- encoding: utf-8
 
 from PIL import Image, ImageDraw
+
+from heart_coords import get_heart_rows, scale, HEART_10, HEART_12
+
 
 im = Image.new("L", size=(100, 100))
 draw = ImageDraw.Draw(im)
 
-for i, line in enumerate(heart10.splitlines()):
-    if not line.strip():
-        continue
-
-    for j, char in enumerate(line.rstrip()):
-        if char == "X":
-            draw.polygon(
-                [
-                    (j * 10, (i - 1) * 10),
-                    (j * 10, i * 10),
-                    ((j + 1) * 10, i * 10),
-                    ((j + 1) * 10, (i - 1) * 10),
-                ],
-                fill="red"
-            )
-        print((i, j), char)
-    # print(i)
-    # ImageDraw.draw(im).polygon
-    # print(line)
+for row in get_heart_rows(HEART_10):
+    for rect in row:
+        draw.polygon(scale(rect, factor=10), fill="red")
 
 im.save("heart10.png")
 
 
-
-heart8 = """
-  XX    XX
- XXXX  XXXX
-XXXXXXXXXXXX
-XXXXXXXXXXXX
-XXXXXXXXXXXX
-XXXXXXXXXXXX
-XXXXXXXXXXXX
- XXXXXXXXXX
-  XXXXXXXX
-   XXXXXX
-    XXXX
-     XX
-"""
-
-from PIL import Image, ImageDraw
-
 im = Image.new("L", size=(120, 120))
 draw = ImageDraw.Draw(im)
 
-for i, line in enumerate(heart8.splitlines()):
-    if not line.strip():
-        continue
+for row in get_heart_rows(HEART_12):
+    for rect in row:
+        draw.polygon(scale(rect, factor=10), fill="red")
 
-    for j, char in enumerate(line.rstrip()):
-        if char == "X":
-            draw.polygon(
-                [
-                    (j * 10, (i - 1) * 10),
-                    (j * 10, i * 10),
-                    ((j + 1) * 10, i * 10),
-                    ((j + 1) * 10, (i - 1) * 10),
-                ],
-                fill="red"
-            )
-        print((i, j), char)
-    # print(i)
-    # ImageDraw.draw(im).polygon
-    # print(line)
-
-im.save("heart8.png")
+im.save("heart12.png")
