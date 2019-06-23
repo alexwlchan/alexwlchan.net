acac69f1e9b755c27ed91307d88df6aaff17a1a0
Alex Chan <alex@alexwlchan.net>
2019-06-23 16:33:30 +0100

    Turn on HTMLProofer

--- src/_plugins/html_proofer.rb
+++ src/_plugins/html_proofer.rb
@@ -1,11 +1,11 @@
 require "html-proofer"
 
 Jekyll::Hooks.register :site, :post_write do |site|
-#   HTMLProofer.check_directory(
-#     site.config["destination"], opts = {
-#       :check_html => false,
-#       :check_img_http => true,
-#       :disable_external => true,
-#       :report_invalid_tags => true,
-#     }).run
+  HTMLProofer.check_directory(
+    site.config["destination"], opts = {
+      :check_html => false,
+      :check_img_http => true,
+      :disable_external => true,
+      :report_invalid_tags => true,
+    }).run
 end
