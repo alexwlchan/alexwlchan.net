48aee18734dc322f770da16338734ea25416587f
Alex Chan <alex@alexwlchan.net>
2019-09-23 08:45:31 +0100

    Fix the linting in 'make build' and 'make build-drafts'

---
+++ src/_plugins/linter.rb
@@ -0,0 +1,25 @@
+require "html-proofer"
+
+
+class RunLinting < Jekyll::Command
+  class << self
+    def init_with_program(prog)
+      prog.command(:lint) do |cmd|
+        cmd.action do |_, options|
+          options = configuration_from_options(options)
+          run_linting(options["destination"])
+        end
+      end
+    end
+
+    def run_linting(dirname)
+      HTMLProofer.check_directory(
+        dirname, opts = {
+          :check_html => false,
+          :check_img_http => true,
+          :disable_external => true,
+          :report_invalid_tags => true,
+        }).run
+    end
+  end
+end
