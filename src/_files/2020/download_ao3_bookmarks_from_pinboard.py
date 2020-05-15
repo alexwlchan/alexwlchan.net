76d3d3c199ac28332aede5e503fde978c7e4f3b7
Alex Chan <alex@alexwlchan.net>
2020-05-15 19:54:01 +0100

    Finish my post about downloading AO3 fics from Pinboard

--- src/_files/2020/download_ao3_bookmarks_from_pinboard.py
+++ src/_files/2020/download_ao3_bookmarks_from_pinboard.py
@@ -1,42 +1,3 @@
----
-layout: post
-title: Downloading the AO3 fics that I've saved in Pinboard
-summary: A script that downloads the nicely formatted AO3 downloads for everything I've saved in Pinboard.
-category: Python
----
-
-A couple of days ago, I tweeted about one of my scripts:
-
-{% tweet https://twitter.com/alexwlchan/status/1260225157672697858 %}
-
-The tweet got a fair amount of interest, so in this post I'm going to share that script.
-
-Pinboard (<https://pinboard.in/>) is a bookmarking service that I've used for many years.
-You can use it to save links so you can find them later, and [if you pay extra for an archival account](https://pinboard.in/tour/#archive), Pinboard will crawl and save a copy of every bookmark in your account.
-If the original site breaks or changes, you can still see the archived copy.
-
-AO3 (the Archive of Our Own, <https://archiveofourown.org/>) is a site that hosts fanworks, primarily fanfiction.
-I save a lot of fic from AO3 in my Pinboard account, and I know I'm not the only one -- a lot of other fannish folk use Pinboard to save bookmarks, in part because Maciej (who runs Pinboard) has made concerted efforts [to win them over](https://idlewords.com/talks/fan_is_a_tool_using_animal.htm).
-
-If you save an AO3 fic to Pinboard, and let the archival account keep an archived copy, it saves the fic as it appears on the web -- embedded in a larger page, including navigation and CSS.
-As a backup, that's good, but better would be to use the exports that AO3 provides:
-
-<img src="/images/2020/ao3_download_button.png" alt="A button labelled 'Download', highlighted in red. Below it are several other buttons labelled AZW3, EPUB, MOBI, PDF and HTML." style="width: 389px;">
-
-These allow you to download a fic without all the page ornaments -- just the text, nothing more.
-It means you can read fic offline or with a device like an e-reader; these exports also serve as much more usable backup copies.
-
-Downloading the exports for every fic you've saved would be very tedious and manual; scripting makes it much faster.
-
-I do most of my scripting in Python, and that's what I used here.
-Specifically, I'm using the [Pinboard API](https://pinboard.in/api) to get a list of my bookmarks, and then going through to find the AO3 links and download all the nicely-formatted exports.
-
-To use it, [download the script](/files/2020/download_ao3_bookmarks_from_pinboard.py) or copy/paste the code below to a file.
-Run the file with Python in a terminal (for example, `python download_bookmarks.py`), enter the API token from [your settings page](https://pinboard.in/settings/password), and watch as a folder fills up with downloaded fic.
-
-If you have issues running this code, get in touch [on Twitter](https://twitter.com/alexwlchan).
-
-```python
 #!/usr/bin/env python
 """
 Download all the fic you've saved in Pinboard.
@@ -170,4 +131,3 @@ if __name__ == "__main__":
 
     for ao3_id in sorted(ao3_identifiers):
         download_work_exports(ao3_id=ao3_id)
-```
