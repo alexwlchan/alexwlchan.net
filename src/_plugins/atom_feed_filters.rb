f2fbb539590a6ea56156a40330e4cbbc5b9117e2
Alex Chan <alex@alexwlchan.net>
2021-01-01 18:39:30 +0000

    Add pretty printing of the XML in the Atom feed
    This should make debugging it easier, and leave less vast swathes of
    whitespace in the file.

--- src/_plugins/atom_feed_filters.rb
+++ src/_plugins/atom_feed_filters.rb
@@ -49,6 +49,13 @@ module Jekyll
 
       doc.to_s
     end
+    
+    def pretty_print_xml(xml)
+      doc = Nokogiri.XML(xml) do |config|
+        config.default_xml.noblanks
+      end
+      doc.to_xml(:indent => 2)
+    end
   end
 end
 
