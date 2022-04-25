ab2b4e68f7b66dc6b95e746bde4bef84c2c7ec17
Alex Chan <alex@alexwlchan.net>
2022-04-25 20:50:03 +0100

    Add a tool for searching tags

---
+++ scripts/search_tags.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# This is a small command-line tool that lets me search the
+# tags on my blog posts.
+#
+# I use it when I'm writing new posts, and I want to see what
+# tags I've used in the past.
+#
+# This requires fzf (https://github.com/junegunn/fzf) to work,
+# which lets me search my tags.
+
+require "yaml"
+
+tally = Hash.new(0)
+
+Dir.glob("src/**/*.md") do |path|
+  # puts path
+  metadata = YAML.load_file(path)
+  tags = metadata["tags"]
+
+  if tags.nil?
+    next
+  end
+
+  metadata["tags"].split(" ").each { |t|
+    tally[t] += 1
+  }
+end
+
+fzf_string = tally
+  .sort_by { |t, _| t }
+  .map { |t, count| "#{t} (#{count})" }
+  .join("\n")
+
+`echo "#{fzf_string}" | fzf`
