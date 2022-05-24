3a0a6408de8a51d6f5b2b239d5e4b58c36381c28
Alex Chan <alex@alexwlchan.net>
2022-05-24 10:22:21 +0100

    Sort the source files for the CSS fingerprint
    For #493

--- src/_plugins/css_fingerprint.rb
+++ src/_plugins/css_fingerprint.rb
@@ -4,7 +4,12 @@
 # together into a single CSS file.  This gets included in the link to
 # the CSS files, so clients can cache the CSS file until it changes.
 
-checksums = Dir.glob("src/_scss/*.scss").map { |f|
+# Note: it's important the source files are in a consistent order.
+# Globbing doesn't seem to return a fixed order, so we sort ourselves.
+# See https://github.com/alexwlchan/alexwlchan.net/issues/493
+source_files = Dir.glob("src/_scss/*.scss").sort
+
+checksums = source_files.map { |f|
   Digest::MD5.file(open f).hexdigest
 }
 
