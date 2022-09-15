d99067bd62dae634b2a54ea6203a2637bb9c474c
Alex Chan <alex@alexwlchan.net>
2022-09-15 22:54:32 +0100

    link to visible tags on pages

--- src/_plugins/tags.rb
+++ src/_plugins/tags.rb
@@ -3,13 +3,14 @@
 module Jekyll
   class TagTally < Generator
     def generate(site)
+
+      # Create a tally of all tags used in visible posts on the site.
+      #
+      # This will be used to decide which tags have been used multiple times,
+      # and which should be shown on posts/in the site wide index.
       tag_tally = Hash.new(0)
 
       site.posts.docs.each { |post|
-
-        # Make sure that posts which are excluded from the site-wide index
-        # don't count towards the tag totals; they don't have visible tags
-        # and won't appear in the tag filters.
         if post.data.fetch("index", {}).fetch("exclude", false)
           next
         end
@@ -17,6 +18,13 @@ module Jekyll
         post.data["tags"].each { |tag|
           tag_tally[tag] += 1
         }
+      }
+
+      # Now we go through the posts a second time, and create a new
+      # 'visibleTags' list of all the tags on this post that appear on
+      # at least one other post.
+      site.posts.docs.each { |post|
+        post.data["visibleTags"] = post.data["tags"].select { |t| tag_tally[t] > 1 };
       }
 
       site.data["tag_tally"] = tag_tally
