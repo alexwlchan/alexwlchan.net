cd6aa0268c9fb5c2d71ab6306861293b0079a777
Alex Chan <alex@alexwlchan.net>
2022-12-19 09:16:54 +0000

    Create multiple sizes of card image

---
+++ src/_plugins/card_images.rb
@@ -0,0 +1,90 @@
+require "fileutils"
+require "rszr"
+require "shellwords"
+
+Jekyll::Hooks.register :site, :post_read do |site|
+  site.posts.docs.each { |p|
+    year = p.date.year
+    slug = p.data["slug"]
+
+    matching_images = Dir["src/_cards/#{year}/#{slug}*"]
+
+    # If there are no cards, then both the social and index card should
+    # use the default image.  Note that the social cards behave slightly
+    # different in this case, so we defer the default to the template.
+    if matching_images.empty?
+      next
+    end
+
+    FileUtils.mkdir_p "_site/images/cards/#{year}"
+
+    # For each matching source file in the card directory, create a 1x/2x
+    # version of the card in the output directory.
+    #
+    # Cards are usually shown at ~350px wide.
+    #
+    # I'm using ImageMagick rather than the rszr gem because I can't seem
+    # to get WebP images from rszr; if I try I get a slightly cryptic error:
+    #
+    #     Rszr::SaveError: Non-existant path component
+    #
+    matching_images.each { |im_path|
+      ext = File.extname(im_path)         # e.g. '.jpg'
+      name = File.basename(im_path, ext)  # e.g. 'marquee-rocket'
+
+      for out_ext in [".webp", ext]
+        out_path_1x = "_site/images/cards/#{year}/#{name}-1x#{out_ext}"
+        out_path_2x = "_site/images/cards/#{year}/#{name}-2x#{out_ext}"
+
+        unless File.exist? out_path_1x
+          `convert #{Shellwords.escape(im_path)} -background none -resize 400x200 #{Shellwords.escape(out_path_1x)}`
+        end
+
+        unless File.exist? out_path_2x
+          `convert #{Shellwords.escape(im_path)} -background none -resize 800x400 #{Shellwords.escape(out_path_2x)}`
+        end
+      end
+    }
+
+    # Now work out which image is which the card for social media, which is
+    # the card for the site-wide index (which may be different).
+    if matching_images.length == 2
+      index_card = matching_images.find { |p| p.include? ".index." }
+      social_card = matching_images.find { |p| p.include? ".social." }
+    else
+      index_card = social_card = matching_images[0]
+    end
+
+    # Now we attach enough data to the post that the downstream components
+    # can render the necessary HTML.
+    p.data["card"] = {
+      "social" => {
+        "name" => File.basename(social_card, File.extname(social_card)),
+        "ext" => File.extname(social_card),
+      },
+      "index" => {
+        "name" => File.basename(index_card, File.extname(index_card)),
+        "ext" => File.extname(index_card),
+      },
+    }
+  }
+end
+
+module Jekyll
+  module MimeTypeFilters
+    def mime_type(ext)
+      case ext
+        when ".png"
+          "image/png"
+        when ".jpeg", ".jpg"
+          "image/jpeg"
+        else
+          STDERR.puts "Unrecognised image extension: #{ext}"
+          exit 1
+      end
+    end
+  end
+end
+
+Liquid::Template::register_filter(Jekyll::MimeTypeFilters)
+
