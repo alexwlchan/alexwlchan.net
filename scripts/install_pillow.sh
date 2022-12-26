dd4c269891b9524d6b5fb5436fe4bb6ede660b3b
Alex Chan <alex@alexwlchan.net>
2022-12-26 23:58:21 +0000

    remove a stray hash

--- scripts/install_pillow.sh
+++ scripts/install_pillow.sh
@@ -8,6 +8,6 @@ apk update
 apk add g++ libavif-dev python3 python3-dev py3-pip py3-pillow
 pip3 install pillow-avif-plugin==1.3.1 tqdm==4.64.1
 
-# # These packages are only required for installation, not for running Pillow
+# These packages are only required for installation, not for running Pillow
 apk del py3-pip
 rm -rf /var/cache/apk/*
