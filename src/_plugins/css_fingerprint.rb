dd1e830f0921c79fe6f0ed27982389e49a5ac608
Alex Chan <alex@alexwlchan.net>
2022-12-31 16:19:29 +0000

    Add a mechanism for injecting extra style sheets

--- src/_plugins/css_fingerprint.rb
+++ src/_plugins/css_fingerprint.rb
@@ -7,7 +7,7 @@
 # Note: it's important the source files are in a consistent order.
 # Globbing doesn't seem to return a fixed order, so we sort ourselves.
 # See https://github.com/alexwlchan/alexwlchan.net/issues/493
-source_files = Dir.glob("src/_scss/*.scss").sort
+source_files = (Dir.glob("src/_scss/**/*.scss") + Dir.glob("src/styles/**/*.scss")).sort
 
 checksums = source_files.map { |f|
   Digest::MD5.file(open f).hexdigest
