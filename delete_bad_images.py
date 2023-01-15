afc5bedf704b9fa4f8093b7adefeebe383f4eb86
Alex Chan <alex@alexwlchan.net>
2023-01-15 11:59:27 +0000

    just blat them all

--- delete_bad_images.py
+++ delete_bad_images.py
@@ -2,5 +2,17 @@
 
 import os
 
-for line in open('paths.txt'):
-    os.unlink(line.strip())
+import os
+
+def get_file_paths_under(root=".", *, suffix=""):
+    """Generates the paths to every file under ``root``."""
+    if not os.path.isdir(root):
+        raise ValueError(f"Cannot find files under non-existent directory: {root!r}")
+
+    for dirpath, _, filenames in os.walk(root):
+        for f in filenames:
+            if os.path.isfile(os.path.join(dirpath, f)) and f.lower().endswith(suffix):
+                yield os.path.join(dirpath, f)
+
+for path in get_file_paths_under(".", suffix=".avif"):
+    os.unlink(path)
