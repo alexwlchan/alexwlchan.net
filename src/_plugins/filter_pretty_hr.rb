d72aa93024677e59e1b7f6991e0322b27acec215
Alex Chan <alex@alexwlchan.net>
2023-01-19 22:32:28 +0000

    let's try freezing this string, making it a constant

--- src/_plugins/filter_pretty_hr.rb
+++ src/_plugins/filter_pretty_hr.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+#
 # Replaces <hr> instances with a slightly prettier version.
 #
 # Based on https://www.sarasoueidan.com/blog/horizontal-rules/
 
+SEPARATOR = <<~HTML
+  <svg viewBox="0 0 17 1" width="100px" role="separator">
+    <rect        width="1" height="1"/>
+    <rect x="4"  width="1" height="1"/>
+    <rect x="8"  width="1" height="1"/>
+    <rect x="12" width="1" height="1"/>
+    <rect x="16" width="1" height="1"/>
+  </svg>
+HTML
+
 module Jekyll
   module HorizontalRuleFilters
     def pretty_hr(input)
-      separator = <<~HTML
-        <svg viewBox="0 0 17 1" width="100px" role="separator">
-          <rect        width="1" height="1"/>
-          <rect x="4"  width="1" height="1"/>
-          <rect x="8"  width="1" height="1"/>
-          <rect x="12" width="1" height="1"/>
-          <rect x="16" width="1" height="1"/>
-        </svg>
-      HTML
-
-      input.gsub(%r{<hr\s*/>}, separator)
+      input.gsub(%r{<hr\s*/>}, SEPARATOR)
     end
   end
 end
