e3ec6305611cea60ab35f390c3a4a7247d67ddd5
Alex Chan <alex@alexwlchan.net>
2023-01-19 22:57:58 +0000

    Use the Jekyll Cache API for faster builds

--- src/_plugins/filter_cleanup_text.rb
+++ src/_plugins/filter_cleanup_text.rb
@@ -12,87 +12,85 @@
 
 module Jekyll
   module CleanupsFilter
-    CLEANUP_TEXT_CACHE = {}
-
-    def cleanup_text(input)
-      CLEANUP_TEXT_CACHE.fetch(input) do |input|
-        CLEANUP_TEXT_CACHE[input] = _do_cleanup_text(input)
-      end
+    def cache
+      @@cache ||= Jekyll::Cache.new("CleanupText")
     end
 
-    def _do_cleanup_text(input)
-      text = input
+    def cleanup_text(input)
+      cache.getset(input) do
+        text = input
 
-      # Add a non-breaking space after words which are followed by
-      # a number, e.g. 'Apollo 11' or 'RFC 456'
-      prefix_words = %w[
-        Apollo
-        HTTP
-        ImageMagick
-        issue
-        RFC
-        Part
-        part
-        Season
-        season
-      ].join('|')
+        # Add a non-breaking space after words which are followed by
+        # a number, e.g. 'Apollo 11' or 'RFC 456'
+        prefix_words = %w[
+          Apollo
+          HTTP
+          ImageMagick
+          issue
+          RFC
+          Part
+          part
+          Season
+          season
+        ].join('|')
 
-      text = text.gsub(/(#{prefix_words}) (\d+)/, '\1&nbsp;\2')
+        text = text.gsub(/(#{prefix_words}) (\d+)/, '\1&nbsp;\2')
 
-      # Add a non-breaking space after words which are preceded by
-      # a number, e.g. '1 second' or '5 bytes'
-      countable_words = %w[
-        Artemis
-        second
-        minute
-        hour
-        character
-        byte
-      ].join('|')
+        # Add a non-breaking space after words which are preceded by
+        # a number, e.g. '1 second' or '5 bytes'
+        countable_words = %w[
+          Artemis
+          second
+          minute
+          hour
+          character
+          byte
+        ].join('|')
 
-      text = text.gsub(/(\d+) (#{countable_words})/, '\1&nbsp;\1')
+        text = text.gsub(/(\d+) (#{countable_words})/, '\1&nbsp;\1')
 
-      # Other phrases which needed non-breaking spaces or non-breaking
-      # dashes.
-      phrases = [
-        '<em>k</em>-means',
-        'CC BY 4.0',
-        'CC BY-NC 4.0',
-        'CC BY-NC-ND',
-        'CC BY-SA 2.0',
-        'CC BY-SA 4.0',
-        'CC BY',
-        'iMac G3',
-        'iPhone X',
-        'JPEG 2000',
-        'Mac OS 9',
-        'P-215',
-        'PyCon ',
-        'Route 53'
-      ]
+        # Other phrases which needed non-breaking spaces or non-breaking
+        # dashes.
+        phrases = [
+          '<em>k</em>-means',
+          'CC BY 4.0',
+          'CC BY-NC 4.0',
+          'CC BY-NC-ND',
+          'CC BY-SA 2.0',
+          'CC BY-SA 4.0',
+          'CC BY',
+          'iMac G3',
+          'iPhone X',
+          'JPEG 2000',
+          'Mac OS 9',
+          'P-215',
+          'PyCon ',
+          'Route 53'
+        ]
 
-      phrases.each do |p|
-        replacement = p.gsub(' ', '&nbsp;').gsub('-', '&#8209;')
-        text = text.gsub(p, replacement)
+        phrases.each do |p|
+          replacement = p.gsub(' ', '&nbsp;').gsub('-', '&#8209;')
+          text = text.gsub(p, replacement)
+        end
+
+        # Display "LaTeX" in a nice way, if you have CSS enabled
+        text = text.gsub(
+          'LaTeX',
+          '<span class="latex">L<sup>a</sup>T<sub>e</sub>X</span>'
+        )
+
+        text = text.gsub(
+          'TeX',
+          '<span class="latex">T<sub>e</sub>X</span>'
+        )
+
+        # Make sure that footnote markers are rendered as a text
+        # arrow on iOS devices, not emoji.  For more info:
+        # http://daringfireball.net/linked/2015/04/22/unicode-emoji
+        text
+          .gsub('&#8617;', '&#8617;&#xFE0E;')
+          .gsub('↩', '&#8617;&#xFE0E;')
       end
-
-      # Display "LaTeX" in a nice way, if you have CSS enabled
-      text = text.gsub(
-        'LaTeX',
-        '<span class="latex">L<sup>a</sup>T<sub>e</sub>X</span>'
-      )
-
-      text = text.gsub(
-        'TeX',
-        '<span class="latex">T<sub>e</sub>X</span>'
-      )
-
-      # Make sure that footnote markers are rendered as a text
-      # arrow on iOS devices, not emoji.  For more info:
-      # http://daringfireball.net/linked/2015/04/22/unicode-emoji
-      text
-        .gsub('&#8617;', '&#8617;&#xFE0E;')
-        .gsub('↩', '&#8617;&#xFE0E;')
     end
   end
 end
