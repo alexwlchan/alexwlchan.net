66037cfd440f9fd16d70a48375ae561ab2d88455
Alex Chan <alex@alexwlchan.net>
2023-01-26 22:20:43 +0000

    Add some tests for filter_cleanup_text; fix a bug

---
+++ src/_tests/test_filter_cleanup_text.rb
@@ -0,0 +1,17 @@
+require 'test/unit'
+
+require_relative '../_plugins/filter_cleanup_text'
+
+class TextCleanupText < Test::Unit::TestCase
+  def test_it_adds_non_breaking_spaces_after_words
+    text = 'Apollo 11 launched in 1969'
+    expected = 'Apollo&nbsp;11 launched in 1969'
+    assert_equal(AddNonBreakingSpaces.add_non_breaking_spaces(text), expected)
+  end
+
+  def test_it_adds_non_breaking_spaces_before_words
+    text = 'It takes 2 minutes'
+    expected = 'It takes 2&nbsp;minutes'
+    assert_equal(AddNonBreakingSpaces.add_non_breaking_spaces(text), expected)
+  end
+end
