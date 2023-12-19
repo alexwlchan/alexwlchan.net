9f553757425e0af85f9155a376e8c6f2026d6ec0
Alex Chan <alex@alexwlchan.net>
2023-12-19 09:44:48 +0000

    Add more debugging

--- src/_plugins/linter.rb
+++ src/_plugins/linter.rb
@@ -219,6 +219,9 @@ class RunLinting < Jekyll::Command
           md_path.start_with?('src/_posts/') or
           md_path.start_with?('src/_drafts/')
 
+        puts md_path
+        puts is_in_post_directory
+
         errors[md_path] <<= "layout should be 'post'; got #{front_matter['layout']}" if is_in_post_directory && front_matter['layout'] != 'post'
 
         errors[md_path] <<= "layout should be 'page'; got #{front_matter['layout']}" if !is_in_post_directory && front_matter['layout'] != 'page'
