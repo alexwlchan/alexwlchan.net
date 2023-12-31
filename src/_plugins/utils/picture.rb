22cda74bcbe09a4a5b5bd7359be6cd6d449e4c8b
Alex Chan <alex@alexwlchan.net>
2023-12-31 08:27:52 +0000

    Test the function for finding colour profiles

---
+++ src/_plugins/utils/picture.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# Generates a map (path) -> (colour profile)
+def get_colour_profiles(dirname)
+  # Flags:
+  #
+  #    -r = recursive, search for every file in the folder
+  #
+  exiftool_output = `exiftool -r -quiet -printFormat '$directory/$filename : $profileDescription' #{dirname}/** 2>&1`
+
+  # There are two possible outputs for the line, of the form:
+  #
+  #     src/_tests/images/gradient-with-p3.png : Display P3
+  #
+  #     Warning: [Minor] Tag 'profileDescription' not defined - src/_tests/images/gradient-with-srgb.png
+  #
+  exiftool_output
+    .split("\n")
+    .to_h do |line|
+      if line.start_with?("Warning: [Minor] Tag 'profileDescription' not defined - ")
+        path = line.split(' - ')[-1]
+        path = path.strip
+
+        [path, nil]
+      else
+        path, profile = line.split(':')
+        path = path.strip!
+        profile = profile.strip!
+
+        [path, profile]
+      end
+    end
+end
