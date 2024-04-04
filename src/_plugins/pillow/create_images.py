196273412804839c0ed79de1afb464257a788d09
Alex Chan <alex@alexwlchan.net>
2024-04-04 09:59:58 +0100

    Make sure we create the image directory first

--- src/_plugins/pillow/create_images.py
+++ src/_plugins/pillow/create_images.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import json
 
 from PIL import Image
@@ -11,6 +12,9 @@ if __name__ == "__main__":
 
         im = Image.open(resize_request['source_path'])
         im = im.resize((resize_request['width'], resize_request['height']))
+
+        os.makedirs(os.path.dirname(resize_request['out_path']), exist_ok=True)
+
         im.save(resize_request['out_path'])
 
         print(resize_request)
