acf8ae123d4bc261de752ea774767549b9f1e2b6
Alex Chan <alex@alexwlchan.net>
2024-04-05 14:12:41 +0100

    Finish breaking up the vague `css.rb` plugin, create one for tint colors

--- src/_plugins/tint_colors.rb
+++ src/_plugins/tint_colors.rb
@@ -8,9 +8,12 @@
 #
 # == Usage ==
 #
-# There is one part to this plugin:
+# There are two parts to this plugin:
 #
-#   1.  A tag you can use in the <head> of a page to get the custom CSS
+#   1.  A pre-render hook that checks there's sufficient contrast between
+#       the chosen tint color and the background of the page.
+#
+#   2.  A tag you can use in the <head> of a page to get the custom CSS
 #       variables (if any) for this page:
 #
 #           {% tint_color_css %}
@@ -20,6 +23,35 @@
 #
 
 require_relative 'utils/contrast'
+
+# Throws an error if the CSS colors on a given page don't have enough
+# contrast with the white/black backgrounds.
+def ensure_sufficient_contrast(page_data)
+  colors = page_data.fetch('colors', {})
+
+  primary_color_light = colors['css_light']
+  primary_color_dark = colors['css_dark']
+
+  if primary_color_light.nil? && primary_color_dark.nil?
+    return
+  end
+
+  if contrast(primary_color_light, '#ffffff') < 4.5
+    throw "light color: insufficient contrast between white and #{primary_color_light}: #{contrast(primary_color_light, '#ffffff')} < 4.5"
+  end
+
+  return unless contrast(primary_color_dark, '#000000') < 4.5
+
+  throw "dark color: insufficient contrast between black and #{primary_color_dark}: #{contrast(primary_color_dark, '#000000')} < 4.5"
+end
+
+Jekyll::Hooks.register :pages, :pre_render do |page|
+  ensure_sufficient_contrast(page.data)
+end
+
+Jekyll::Hooks.register :documents, :pre_render do |doc|
+  ensure_sufficient_contrast(doc.data)
+end
 
 module Jekyll
   class TintColorCssTag < Liquid::Tag
