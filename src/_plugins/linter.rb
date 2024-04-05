5567ab538e519dd7535214a22a7a9d7f7a676d75
Alex Chan <alex@alexwlchan.net>
2024-04-05 22:13:17 +0100

    Continue fiddling to make the linter logic correct

--- src/_plugins/linter.rb
+++ src/_plugins/linter.rb
@@ -217,10 +217,14 @@ class RunLinting < Jekyll::Command
 
         errors[md_path] = md_errors unless md_errors.empty?
 
-        if (md_path.start_with?('src/_posts/') || md_path.start_with?('src/_drafts')) && front_matter['layout'] != 'post'
-          errors[md_path] <<= "layout should be 'post'; got #{front_matter['layout']}"
-        elsif md_path.start_with?('src/_til/') && front_matter['layout'] != 'til'
-          errors[md_path] <<= "layout should be 'til'; got #{front_matter['layout']}"
+        if (md_path.start_with?('src/_posts/') || md_path.start_with?('src/_drafts'))
+          if front_matter['layout'] != 'post'
+            errors[md_path] <<= "layout should be 'post'; got #{front_matter['layout']}"
+          end
+        elsif md_path.start_with?('src/_til/')
+          if front_matter['layout'] != 'til'
+            errors[md_path] <<= "layout should be 'til'; got #{front_matter['layout']}"
+          end
         elsif front_matter['layout'] != 'page'
           errors[md_path] <<= "layout should be 'page'; got #{front_matter['layout']}"
         end
