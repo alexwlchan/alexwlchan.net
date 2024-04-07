53fc1190303ead871454b7d2936cbfc2ce3155b5
Alex Chan <alex@alexwlchan.net>
2024-04-07 09:19:37 +0100

    Continue to trim the size of the article cards
    Looking at /articles/ in particular:
    
    Before: 365,315 bytes
    After:  345,804 bytes

--- src/_plugins/card_images.rb
+++ src/_plugins/card_images.rb
@@ -118,7 +118,7 @@ module Jekyll
             width="400"
             alt=""
             loading="lazy"
-            class="card_image"
+            class="c_image"
             #{attribution_text}
             data-proofer-ignore
           %}
