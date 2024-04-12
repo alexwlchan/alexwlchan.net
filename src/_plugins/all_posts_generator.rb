81ed6606defd38e31e94040aaa9faab6171dc083
Alex Chan <alex@alexwlchan.net>
2024-04-12 14:52:28 +0100

    Remove a now-unused plugin

--- src/_plugins/all_posts_generator.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-  class AllPostsByYear < Generator
-    def generate(site)
-      site.data['posts_by_year'] =
-        site.posts.docs.reverse_each.group_by { |post| post.date.year }
-    end
-  end
-end
