1b82499d31b55489dabca976fadfda722f0f3517
Alex Chan <alex@alexwlchan.net>
2024-04-13 05:08:58 +0100

    Push tags into the new meta template

--- src/_plugins/visible_tags.rb
+++ src/_plugins/visible_tags.rb
@@ -21,7 +21,18 @@ def create_visible_tags(collection_of_docs)
   visible_tags = tag_tally.select { |_, count| count > 1 }.keys.to_set
 
   collection_of_docs.each do |doc|
-    doc.data['visible_tags'] = doc.data['tags'].select { |t| visible_tags.include? t }
+    # I deliberately don't have visible tags from pages that are unlisted,
+    # so you don't get a confusing navigation flow where:
+    #
+    # 1.  User lands on page
+    # 2.  They click on the tags to find similar pages
+    # 3.  The page they were just on isn't listed
+    #
+    doc.data['visible_tags'] = if doc.data.fetch('index', {}).fetch('exclude', false)
+                                 []
+                               else
+                                 doc.data['tags'].select { |t| visible_tags.include? t }
+                               end
   end
 end
 
