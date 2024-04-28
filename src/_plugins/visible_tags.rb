a5e1e9eb18b5cf48c5217fa0235aa662634ae888
Alex Chan <alex@alexwlchan.net>
2024-04-28 19:37:47 +0100

    Tidy up visible_tags.rb a bit
    * Reduce the perceived complexity
    * Use the nice `Enumerable.tally` method
    * Pull out the `visible?` logic into a standalone function

--- src/_plugins/visible_tags.rb
+++ src/_plugins/visible_tags.rb
@@ -2,40 +2,43 @@
 
 # I use colons for namespacing my tags (e.g. `python` and `python:pillow`).
 #
-# Whenever I tag an article with a namespaced tag, I always tag it with the
-# parent -- e.g. an article tagged `python:pillow` is always tagged with
+# Whenever I tag a post with a namespaced tag, I always tag it with the
+# parent -- e.g. a post tagged `python:pillow` is always tagged with
 # `python` also.  But because those tags are less useful on their own,
 # I don't expose namespaced tags unless they're used at least three times.
 #
-# This plugin adds a `visible_tags` field to every article, which is
+# This plugin adds a `visible_tags` field to every post, which is
 # limited to tags with more than one use.
 
+# Returns true if a tag is visible, false otherwise
+def visible?(tag_tally, tag_name)
+  is_namespaced = tag_name.include?(':')
+
+  if is_namespaced
+    tag_tally[tag_name] >= 3
+  else
+    true
+  end
+end
+
 def create_visible_tags(collection_of_docs)
-  tag_tally = Hash.new(0)
-
-  collection_of_docs.each do |doc|
-    next if doc.data.fetch('index', {}).fetch('exclude', false)
-
-    doc.data['tags'].each do |tag|
-      tag_tally[tag] += 1
-    end
+  # I deliberately don't have visible tags from posts that are unlisted,
+  # so you don't get a confusing navigation flow where:
+  #
+  # 1.  User lands on post
+  # 2.  They click on the tags to find similar posts
+  # 3.  The post they were just on isn't listed
+  #
+  visible_docs = collection_of_docs.reject do |doc|
+    doc.data.fetch('index', {}).fetch('exclude', false)
   end
 
-  visible_tags = tag_tally.select { |tag_name, count| !tag_name.include?(':') or count > 2 }.keys.to_set
+  tag_tally = visible_docs.flat_map { |doc| doc.data['tags'] }.tally
 
-  collection_of_docs.each do |doc|
-    # I deliberately don't have visible tags from pages that are unlisted,
-    # so you don't get a confusing navigation flow where:
-    #
-    # 1.  User lands on page
-    # 2.  They click on the tags to find similar pages
-    # 3.  The page they were just on isn't listed
-    #
-    doc.data['visible_tags'] = if doc.data.fetch('index', {}).fetch('exclude', false)
-                                 []
-                               else
-                                 doc.data['tags'].select { |t| visible_tags.include? t }
-                               end
+  visible_docs.each do |doc|
+    doc.data['visible_tags'] = doc.data['tags'].filter do |tag_name|
+      visible?(tag_tally, tag_name)
+    end
   end
 end
 
