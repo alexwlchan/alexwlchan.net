7c7700244bb7b0697e95cc5a986ef4f82a50a7bc
Alex Chan <alex@alexwlchan.net>
2024-04-29 22:09:39 +0100

    Simplify this method to reuse code

--- src/_plugins/pillow/convert_image.rb
+++ src/_plugins/pillow/convert_image.rb
@@ -2,10 +2,7 @@ require 'json'
 require 'open3'
 
 def convert_image(request)
-  return if File.exist? request['out_path']
-
-  _, status = Open3.capture2('python3', 'src/_plugins/pillow/convert_image.py', JSON.generate(request))
-  raise "Unable to resize image #{request}" unless status.success?
+  convert_multiple_images([request])
 end
 
 def convert_multiple_images(requests)
