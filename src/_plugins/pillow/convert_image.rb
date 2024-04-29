a0e2a610198fb370583457822ac48df6b6d98859
Alex Chan <alex@alexwlchan.net>
2024-04-29 22:02:27 +0100

    Convert multiple images at once in the picture plugin
    In some informal testing, converting the images in bulk is ~2x faster.

--- src/_plugins/pillow/convert_image.rb
+++ src/_plugins/pillow/convert_image.rb
@@ -7,3 +7,15 @@ def convert_image(request)
   _, status = Open3.capture2('python3', 'src/_plugins/pillow/convert_image.py', JSON.generate(request))
   raise "Unable to resize image #{request}" unless status.success?
 end
+
+def convert_multiple_images(requests)
+  json_requests = requests.reject { |req| File.exist? req['out_path'] }
+                          .map { |req| JSON.generate(req) }
+
+  if json_requests.empty?
+    return
+  end
+
+  _, status = Open3.capture2('python3', 'src/_plugins/pillow/convert_image.py', *json_requests)
+  raise "Unable to process requests #{json_requests}" unless status.success?
+end
