fe5455df14b06aa039a5a98cb0a327ed10251f64
Alex Chan <alex@alexwlchan.net>
2024-05-23 21:44:11 +0100

    Add a comment; fix some RuboCop warnings

--- scripts/get_latest_picture.rb
+++ scripts/get_latest_picture.rb
@@ -1,19 +1,26 @@
 #!/usr/bin/env ruby
+# This script creates a basic {% picture %} plugin tag for the
+# newest image in the `_images` folder.
+#
+# It's meant for use in text expansion macros.
+#
+# I'm not currently using it, but I'm keeping it here as something
+# I might want to reuse in future.
 
 require 'date'
 
 images_folder = "src/_images/#{Date.today.year}"
 
-newest_filename, _ = Dir.entries(images_folder)
-  .select { |f| File.file?(File.join(images_folder, f)) }
-  .map { |f| [f, File.mtime(File.join(images_folder, f))]}
-  .max_by { |f, mtime| mtime }
+newest_filename, = Dir.entries(images_folder)
+                      .select { |f| File.file?(File.join(images_folder, f)) }
+                      .map { |f| [f, File.mtime(File.join(images_folder, f))] }
+                      .max_by { |_f, mtime| mtime }
 
-puts <<~EOF
-{%
-  picture
-  filename="#{newest_filename}"
-  width="750"
-  alt=""
-%}
-EOF
+puts <<~MD
+  {%
+    picture
+    filename="#{newest_filename}"
+    width="750"
+    alt=""
+  %}
+MD
