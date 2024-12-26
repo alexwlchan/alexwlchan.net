6d25c59131f0870dbd5d25d6ed573ebbdb40732c
Alex Chan <alex@alexwlchan.net>
2024-12-26 09:02:55 +0000

    Add a quick script to count my posts

---
+++ scripts/count_posts.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# Count the number of posts I've published each year.
+
+require 'date'
+
+this_year = Date.today.year
+
+articles = 0
+til = 0
+
+puts "     articles      TIL    total"
+puts "==== ======== ======== ========"
+
+(2012..Date.today.year).each do |year|
+  article_count = Dir["src/_posts/#{year}/*.md"].length
+  til_count = Dir["src/_til/#{year}/*.md"].length
+
+  puts "#{year}\t#{article_count.to_s.rjust(5)}\t#{til_count.to_s.rjust(6)}\t#{(article_count + til_count).to_s.rjust(7)}"
+
+  articles += article_count
+  til += til_count
+end
+
+puts "==== ======== ======== ========"
+puts "    \t#{articles.to_s.rjust(5)}\t#{til.to_s.rjust(6)}\t#{(articles + til).to_s.rjust(7)}"
