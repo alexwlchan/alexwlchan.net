eeb9f99f6618a07af1bade3822e93e61c8d5f6b8
Alex Chan <alex@alexwlchan.net>
2025-03-07 18:10:25 +0000

    Run RuboCop over the plugins again

--- src/_plugins/article_cards.rb
+++ src/_plugins/article_cards.rb
@@ -100,8 +100,8 @@ Jekyll::Hooks.register :site, :post_read do |site|
   end
 
   posts_with_index_cards = site.posts.docs
-    .reject { |p| p.data['card'].nil? }
-    .filter { |post| post.data.fetch('index', {}).fetch('feature', false) }
+                               .reject { |p| p.data['card'].nil? }
+                               .filter { |post| post.data.fetch('index', {}).fetch('feature', false) }
 
   # Set the "is_new" attribute on any posts which were published
   # in the last few weeks.
