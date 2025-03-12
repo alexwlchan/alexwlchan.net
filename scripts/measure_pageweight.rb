dddf18237ea6a29fada64b9849b636b82619651b
Alex Chan <alex@alexwlchan.net>
2025-03-12 20:51:04 +0000

    Measure the size of key pages

--- scripts/measure_pageweight.rb
+++ scripts/measure_pageweight.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+puts format('Homepage (/):           %2.1f KiB', (File.size('_site/index.html') / 1024.0))
+puts format('Articles (/articles/): %3.1f KiB', (File.size('_site/articles/index.html') / 1024.0))
+
 sizes = []
 
 Dir.glob('_site/**/*.html').each do |f|
@@ -10,4 +13,4 @@ Dir.glob('_site/**/*.html').each do |f|
   sizes.push(File.size(f))
 end
 
-puts format('The average page size %.3f KiB', (sizes.sum / sizes.length / 1024.0))
+puts format('Global average:         %2.1f KiB', (sizes.sum / sizes.length / 1024.0))
