29e035f15b590b09a34dc8b2fb01131d3bb050e8
Alex Chan <alex@alexwlchan.net>
2025-05-28 20:21:11 +0100

    Remove CSS classes from unstyled code blocks
    This reduces the average page size by 0.2 KiB, and the TIL index by
    6.6 KiB -- 15% of the previous page size!

--- src/_plugins/compress_html.rb
+++ src/_plugins/compress_html.rb
@@ -6,8 +6,16 @@ module Jekyll
       cache = Jekyll::Cache.new('CompressHtml')
 
       cache.getset(html) do
+        # The syntax highlighter will add these classes to my HTML
+        # by default, but these are unstyled code blocks -- I don't
+        # need the classes.  Removing them reduces the size of the
+        # final HTML, especially in the TIL index.
+        html = html.gsub('<code class="language-plaintext highlighter-rouge">', '<code>')
+
         compressor = HtmlCompressor::Compressor.new
-        compressor.compress(html)
+        html = compressor.compress(html)
+
+        html
       end
     end
   end
