95a63ccec3beec0ea7fe93947a875140624999a1
Alex Chan <alex@alexwlchan.net>
2025-05-28 20:33:48 +0100

    Remove the code attributes inside `cleanup_text`
    This means they also get applied to the description text in the JSON on
    the homepage list of "favourite posts".

--- src/_plugins/compress_html.rb
+++ src/_plugins/compress_html.rb
@@ -6,21 +6,8 @@ module Jekyll
       cache = Jekyll::Cache.new('CompressHtml')
 
       cache.getset(html) do
-        # The syntax highlighter adds a couple of classes to my HTML,
-        # but I don't have any CSS that targets those classes.
-        #
-        # Removing them reduces the size of the final HTML, especially
-        # in the TIL index.
-        #
-        # The one class I leave behind is `language-console`, which I
-        # have some special styles for.
-        html = html.gsub(' class="language-console highlighter-rouge"', 'class="language-console"')
-        html = html.gsub(/ class="language\-[a-z]+ highlighter\-rouge"/, '')
-
         compressor = HtmlCompressor::Compressor.new
-        html = compressor.compress(html)
-
-        html
+        compressor.compress(html)
       end
     end
   end
