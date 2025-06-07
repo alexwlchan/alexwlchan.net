1c01624aae71dc1573efd7394da8fed7764865f2
Alex Chan <alex@alexwlchan.net>
2025-06-07 21:11:08 +0100

    Update the uptime tests for the changes to the analytics dashboard
    See https://github.com/alexwlchan/analytics.alexwlchan.net/issues/146

--- tests/test_analytics.rb
+++ tests/test_analytics.rb
@@ -8,10 +8,17 @@ require_relative 'utils'
 
 # Tests for analytics.alexwlchan.net
 class TestAnalytics < Test::Unit::TestCase
+  # The analytics app is running
+  def test_homepage_is_up
+    resp = get_url('https://analytics.alexwlchan.net/')
+
+    assert_equal resp.code, '200'
+  end
+
   # If you try to load the dashboard as a public user, you can't see it
   def test_dashboard_is_protected
     resp = get_url('https://analytics.alexwlchan.net/dashboard/')
 
-    assert_equal resp.code, '401'
+    assert_equal resp.code, '302'
   end
 end
