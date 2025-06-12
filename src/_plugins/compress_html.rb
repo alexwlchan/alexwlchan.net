6361d6499e01f712843ae04cd3bcacf43dc17b0e
Alex Chan <alex@alexwlchan.net>
2025-06-12 22:17:26 +0100

    Add a bit more CSS compression

--- src/_plugins/compress_html.rb
+++ src/_plugins/compress_html.rb
@@ -6,10 +6,29 @@ module Jekyll
       cache = Jekyll::Cache.new('CompressHtml')
 
       cache.getset(html) do
-        compressor = HtmlCompressor::Compressor.new
+        compressor = HtmlCompressor::Compressor.new({
+          :compress_css => true,
+          :css_compressor => CssCompressor.new(@context),
+        })
         compressor.compress(html)
       end
     end
+  end
+end
+
+# Use the Sass library to compress a block of CSS.
+#
+# In practice, this is only compressing the tint color variables
+# in the <head> of the page, because all other inline <style> blocks
+# are already being compressed -- but it doesn't hurt!
+class CssCompressor
+  def initialize(context)
+    site = context.registers[:site]
+    @converter = site.find_converter_instance(Jekyll::Converters::Scss)
+  end
+  
+  def compress(source)
+    @converter.convert(source)
   end
 end
 
