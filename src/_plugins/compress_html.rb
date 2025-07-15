ee19ddb2037d851ca0a8ff051fd13e81b1e6ce79
Alex Chan <alex@alexwlchan.net>
2025-07-15 17:45:18 +0100

    plugins: fix a RuboCop issue

--- src/_plugins/compress_html.rb
+++ src/_plugins/compress_html.rb
@@ -7,11 +7,11 @@ module Jekyll
 
       cache.getset(html) do
         minify_html(html, {
-          keep_html_and_head_opening_tags: true,
-          keep_closing_tags: true,
-          minify_css: true,
-          minify_js: true
-        })
+                      keep_html_and_head_opening_tags: true,
+                      keep_closing_tags: true,
+                      minify_css: true,
+                      minify_js: true
+                    })
       end
     end
   end
