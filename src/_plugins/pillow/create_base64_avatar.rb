74a620525adbc3f3469600757b730d35d362475b
Alex Chan <alex@alexwlchan.net>
2025-07-15 16:28:05 +0100

    plugins: add some extra caching to image plugins for faster builds

--- src/_plugins/pillow/create_base64_avatar.rb
+++ src/_plugins/pillow/create_base64_avatar.rb
@@ -2,8 +2,13 @@ require 'json'
 require 'open3'
 
 def create_base64_avatar(path, size)
-  stdout, status = Open3.capture2('python3', 'src/_plugins/pillow/create_base64_avatar.py', path, size.to_s)
-  raise "Unable to create base64 info for #{path}" unless status.success?
+  cache = Jekyll::Cache.new('Base64Avatars')
+  mtime = File.mtime(path).to_i
 
-  stdout
+  cache.getset("#{path}--#{mtime}--#{size}") do
+    stdout, status = Open3.capture2('python3', 'src/_plugins/pillow/create_base64_avatar.py', path, size.to_s)
+    raise "Unable to create base64 info for #{path}" unless status.success?
+
+    stdout
+  end
 end
