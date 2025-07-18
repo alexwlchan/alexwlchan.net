b6db5c4452f0133b76c4fe2c53e731bc6688c82c
Alex Chan <alex@alexwlchan.net>
2025-07-18 10:57:28 +0100

    css: use 4-space tabs for Caddy

--- src/_plugins/cleanup_text.rb
+++ src/_plugins/cleanup_text.rb
@@ -143,7 +143,7 @@ end
 def cleanup_syntax_highlighter_classes(html)
   # I never use the `highlighter-rouge` class, and I only have styles
   # for a couple of the `language-*` classes.
-  %w[console go irb].each do |lang|
+  %w[caddy console go irb].each do |lang|
     html = html.gsub(" class=\"language-#{lang} highlighter-rouge\"", " class=\"language-#{lang}\"")
   end
 
