77e90f1256f69733106b083c7d51722d25522f82
Alex Chan <alex@alexwlchan.net>
2025-12-24 09:09:01 +0000

    plugins: remove some unused code

--- src/_plugins/tint_colors.rb
+++ src/_plugins/tint_colors.rb
@@ -45,16 +45,6 @@ def ensure_sufficient_contrast(css_colors)
   end
 end
 
-# This hooks assigns CSS colors to every page.
-#
-# The colors are either:
-#
-#   - from the YAML front matter in the Markdown file, or
-#   - the default values
-#
-# def ensure_has_css_colors(doc)
-# end
-
 Jekyll::Hooks.register :pages, :pre_render do |page|
   create_asset_images(page)
 end
