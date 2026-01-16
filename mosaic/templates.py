ee55cf95febd01f6f796f55a2d7b65a27dacd950
Alex Chan <alex@alexwlchan.net>
2026-01-16 21:53:23 +0000

    mosaic: add a filter to extract inline styles
    For #1196

--- mosaic/templates.py
+++ mosaic/templates.py
@@ -2,8 +2,11 @@
 Code for dealing with HTML and XML templates.
 """
 
+import collections
 import re
+from typing import TypedDict
 
+import bs4
 from markdown import markdown
 
 
@@ -195,3 +198,36 @@ def force_text_footnote_markers(html: str) -> str:
     """
     # See https://mts.io/2015/04/21/unicode-symbol-render-text-emoji/
     return html.replace("&#8617;", "&#8617;&#xFE0E;").replace("↩", "&#8617;&#xFE0E;")
+
+
+ParsedStyles = TypedDict("ParsedStyles", {"html": str, "styles": str})
+
+
+def get_inline_styles(html: str) -> ParsedStyles:
+    """
+    Find inline <style> tags in an HTML document, and extract them.
+    """
+    if "<style" not in html:
+        return {"html": html, "styles": ""}
+
+    # Contents of <style> tags we've discovered
+    styles = collections.OrderedDict[str, None]()
+
+    # Parse the document as HTML, and look for <style> tags.
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    for style_tag in soup.find_all("style"):
+        styles[style_tag.text.strip()] = None
+
+    # Remove all the <style> tags. We do this as a string manipulation
+    # to avoid bs4 changing the meaning or behaviour of our HTML.
+    for css in styles:
+        html = re.sub(
+            r"\s*<style[^>]*>\s*" + re.escape(css) + r"\s*</style>\s*", "", html
+        )
+
+    # If removing the <style> tags has rendered a set of <defs> empty,
+    # just remove them.
+    if "<defs>" in html:
+        html = re.sub(r"\s*<defs>\s*</defs>\s*", "", html)
+
+    return {"html": html, "styles": "".join(styles)}
