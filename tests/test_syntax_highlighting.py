aff20f619e901340c128e6d5b819e974f8bb475d
Alex Chan <alex@alexwlchan.net>
2026-01-20 20:00:52 +0000

    mosaic: first pass of syntax highlighting in Python
    This adds a Python-Markdown extension which handles fenced code blocks
    with arguments. I don't use the arguments yet, which I'll do in a follow-up
    patch -- but this lays the groundwork for that.
    
    For #1196

---
+++ tests/test_syntax_highlighting.py
@@ -0,0 +1,78 @@
+"""
+Tests for `mosaic.syntax_highlighting`.
+"""
+
+from markdown import markdown
+
+from mosaic.syntax_highlighting import SyntaxHighlighterExtension
+
+
+def test_syntax_highlighting() -> None:
+    """
+    Test a snippet of syntax highlighting.
+    """
+    html = markdown(
+        "This is some text\n\n"
+        '```python {"debug": true}\n'
+        "def greeting(name: str) -> None:\n"
+        '    print(f"Hello {name}!")\n'
+        "```\n\n"
+        "This is some more text\n\n"
+        "*   This is a bulleted list\n\n"
+        "    ```python\n"
+        "    def add(x: int, y: int) -> int:\n"
+        "        return x + y\n"
+        "    ```",
+        extensions=[SyntaxHighlighterExtension()],
+    )
+
+    assert html == (
+        # This is some text
+        "<p>This is some text</p>\n"
+        '<pre class="lng-python"><code>'
+        # def
+        '<span class="k">def</span><span class="w"> </span>'
+        # greeting
+        '<span class="nf">greeting</span>'
+        # (name
+        '<span class="p">(</span><span class="n">name</span>'
+        # : str)
+        '<span class="p">:</span> <span class="nb">str</span><span class="p">)</span> '
+        # -> None
+        '<span class="o">-&gt;</span> <span class="kc">None</span>'
+        # :
+        '<span class="p">:</span>\n    '
+        # print(
+        '<span class="nb">print</span><span class="p">(</span>'
+        # "Hello
+        '<span class="sa">f</span><span class="s2">&quot;Hello </span>'
+        # {name}
+        '<span class="si">{</span><span class="n">name</span><span class="si">}</span>'
+        # !")
+        '<span class="s2">!&quot;</span><span class="p">)</span>\n'
+        "</code></pre>\n\n"
+        # This is some more text
+        "<p>This is some more text</p>\n<ul>\n<li>\n"
+        # This is a bulleted list
+        "<p>This is a bulleted list</p>\n<p>"
+        '<pre class="lng-python"><code>'
+        # def
+        '<span class="k">def</span><span class="w"> </span>'
+        # add(
+        '<span class="nf">add</span><span class="p">(</span>'
+        # x: int
+        '<span class="n">x</span><span class="p">:</span> <span class="nb">int</span>'
+        # , y:
+        '<span class="p">,</span> <span class="n">y</span><span class="p">:</span> '
+        # int)
+        '<span class="nb">int</span><span class="p">)</span> '
+        # ->
+        '<span class="o">-&gt;</span> '
+        # int:
+        '<span class="nb">int</span><span class="p">:</span>\n    '
+        # return x +
+        '<span class="k">return</span> <span class="n">x</span> '
+        # + y
+        '<span class="o">+</span> <span class="n">y</span>\n</code></pre>'
+        "</p>\n</li>\n</ul>"
+    )
