3aede3f86531321a1df8df8b9e3b854c3a365404
Alex Chan <alex@alexwlchan.net>
2026-01-22 12:39:35 +0000

    mosaic/{linters,text}: fix block elements inside `<p>`
    I've found a number of issues with where Python-Markdown inserts `<p>`
    tags, especially around block elements which are inside an indented list.
    
    This patch adds a manual fix to the Markdown converter, and a new lint
    that checks for a `<p>` followed by a block element or other malformed syntax.
    
    For #1196

--- mosaic/linters.py
+++ mosaic/linters.py
@@ -2,8 +2,41 @@
 Linter rules for verifying the final website output.
 """
 
+import re
+
 from bs4 import BeautifulSoup
 import hyperlink
+
+
+def check_no_broken_html(html_str: str) -> list[str]:
+    """
+    Check an HTML file doesn't have any <p> tags followed by unexpected
+    HTML, which is often a sign of a rendering error.
+    """
+    errors = []
+
+    for m in re.finditer(r"<p><(?P<tag_name>[^\s>/]+)(.*?)/?>", html_str):
+        tag_name = m.group("tag_name")
+
+        if tag_name in {
+            "a",
+            "br",
+            "cite",
+            "code",
+            "em",
+            "img",
+            "picture",
+            "s",
+            "strong",
+        }:
+            continue
+
+        errors.append(f"unexpected tag following <p>: {m.group(0)}")
+
+    for m in re.finditer(r"<p>&lt;(.*?)&gt;", html_str):
+        errors.append(f"malformed tag following <p>: {m.group(0)}")
+
+    return errors
 
 
 def check_no_localhost_links(html: BeautifulSoup) -> list[str]:
