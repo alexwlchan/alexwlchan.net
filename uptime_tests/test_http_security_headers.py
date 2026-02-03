858a27b2779477392f4e35df569310f08c010f10
Alex Chan <alex@alexwlchan.net>
2026-02-03 22:36:47 +0000

    uptime_tests: remove tests for now-defunct books site

--- uptime_tests/test_http_security_headers.py
+++ uptime_tests/test_http_security_headers.py
@@ -31,29 +31,3 @@ def test_alexwlchan() -> None:
     assert resp.headers["X-Content-Type-Options"] == "nosniff"
     assert resp.headers["X-Frame-Options"] == "ALLOWALL"
     assert resp.headers["X-Xss-Protection"] == "1; mode=block"
-
-
-def test_books() -> None:
-    """
-    Test security headers for books.alexwlchan.net.
-    """
-    resp = httpx.get("https://books.alexwlchan.net")
-
-    assert resp.headers["Content-Security-Policy"] == (
-        "default-src ; style-src 'self' 'unsafe-inline'; "
-        "img-src 'self' data:; "
-        "script-src 'self' 'unsafe-inline' https://unpkg.com/;connect-src https://analytics.alexwlchan.net/;"
-    )
-    assert resp.headers["Permissions-Policy"] == (
-        "geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), "
-        "microphone=(), camera=(), magnetometer=(), gyroscope=(), vibrate=(), "
-        "payment=()"
-    )
-    assert resp.headers["Referrer-Policy"] == "no-referrer-when-downgrade"
-    assert (
-        resp.headers["Strict-Transport-Security"]
-        == "max-age=31536000; includeSubDomains"
-    )
-    assert resp.headers["X-Content-Type-Options"] == "nosniff"
-    assert resp.headers["X-Frame-Options"] == "DENY"
-    assert resp.headers["X-Xss-Protection"] == "1; mode=block"
