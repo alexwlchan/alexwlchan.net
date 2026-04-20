26272a4b9dc4ec1d300013d32fc16e57f9d67756
Alex Chan <alex@alexwlchan.net>
2026-04-20 05:46:00 +0100

    scripts: switch to my `run_command` runner for less repetition

--- scripts/run_mosaic_tests.sh
+++ scripts/run_mosaic_tests.sh
@@ -3,44 +3,26 @@
 set -o errexit
 set -o nounset
 
-print_info() {
-    echo -e "\033[34m$1\033[0m"
+# Print a command in blue, then run the command
+run_command() {
+    echo ""
+    echo -e "\033[34m-> $@\033[0m"
+    bash -c "$@"
 }
 
-print_info "-> ruff format mosaic scripts tests uptime_tests"
-ruff format mosaic scripts tests uptime_tests
-
-echo ""
+run_command 'ruff format mosaic scripts tests uptime_tests'
 
 if [[ "${CI:-}" == "true" ]]
 then
-  print_info "-> git diff --exit-code"
-  git diff --exit-code
-
-  echo ""
+  run_command 'git diff --exit-code'
 fi
 
-print_info "-> ruff check --fix mosaic scripts tests uptime_tests"
-ruff check --fix mosaic scripts tests uptime_tests
-
-echo ""
-
-print_info "-> mypy mosaic scripts tests"
-mypy mosaic scripts tests
-
-echo ""
+run_command 'ruff check --fix mosaic scripts tests uptime_tests'
+run_command 'mypy mosaic scripts tests'
 
 # These have to be separate so Mypy doesn't get confused by the two
 # files named `conftest.py`
-print_info "-> mypy uptime_tests"
-mypy uptime_tests
+run_command "mypy uptime_tests"
 
-echo ""
-
-print_info "-> python3 -m pytest tests --cov=mosaic -n=auto"
-python3 -m pytest tests --cov=mosaic -n=auto
-
-echo ""
-
-print_info "-> coverage report --include 'mosaic/*,tests/*'"
-coverage report --include 'mosaic/*,tests/*'
+run_command "python3 -m pytest tests --cov=mosaic -n=auto"
+run_command "coverage report --include 'mosaic/*,tests/*'"
