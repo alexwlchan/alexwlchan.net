872cc54c5e68c32c010d59dd665216dc72dbd769
Alex Chan <alex@alexwlchan.net>
2026-05-14 07:16:07 +0100

    scripts: add tools to deploy locally

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+# Print a command in blue, then run the command
+run_command() {
+    echo ""
+    echo -e "\033[34m-> $@\033[0m"
+    bash -c "$@"
+}
+
+run_command 'rm -vf .mosaic_cache.db'
+run_command 'python3 scripts/build_site.py'
+run_command 'rsync --compress --recursive --delete --verbose --checksum --exclude=.DS_Store _out/ linode-vps:repos/alexwlchan.net/_site/'
---
+++ scripts/run_deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# run_deploy.sh is a script that be run any time you want to kick off
+# a deployment of the website.
+#
+# It deliberately doesn't print much and instead directs output to a file
+# which can be inspected later.
+
+set -o errexit
+set -o nounset
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR/.."
+
+source .venv/bin/activate
+
+NOW=$(date +'%Y-%m-%dT%H-%M-%S')
+LOG_NAME="deploy.$NOW.log"
+LOG_PATH="$TMPDIR/$LOG_NAME"
+
+echo "Running deployment"
+echo "Follow logs: tail -f \$TMPDIR/$LOG_NAME"
+
+if bash "$SCRIPT_DIR/deploy.sh" > $LOG_PATH 2>&1; then
+  print_success "Deployment succeeded!"
+else
+  print_error "Deployment failed failed!"
+  cp "$LOG_PATH" ~/Desktop/alexwlchan.net.deploy.$NOW.failed.log
+  print_error "See logs: cat ~/Desktop/alexwlchan.net.deploy.$NOW.failed.log"
+  exit 1
+fi
