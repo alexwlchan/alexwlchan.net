3e0243290fbf4c17b51931c3486f882fbcc53d67
Alex Chan <alex@alexwlchan.net>
2026-05-18 10:01:51 +0100

    scripts: pull from main before deploying
    We should always be up-to-date from the preflight tests, but pull anyway
    in case those were skipped.

--- scripts/run_deploy.sh
+++ scripts/run_deploy.sh
@@ -11,6 +11,8 @@ set -o nounset
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/.."
 
+git pull origin main
+
 source .venv/bin/activate
 
 NOW=$(date +'%Y-%m-%dT%H-%M-%S')
