21fd02c1eaf996774036e1ac7b3918e80224f14f
Alex Chan <alex@alexwlchan.net>
2026-05-24 18:02:33 +0100

    templates: ditch the BeautifulSoup dependency for the TOC

--- mosaic/templates/table_of_contents.py
+++ mosaic/templates/table_of_contents.py
@@ -2,9 +2,9 @@
 Add a table of contents when you insert {% table_of_contents %}.
 """
 
-from typing import TypedDict
+from html.parser import HTMLParser
+from typing import TypeAlias, TypedDict
 
-from bs4 import BeautifulSoup
 from jinja2 import nodes, pass_context
 from jinja2.ext import Extension
 from jinja2.parser import Parser
@@ -49,31 +49,72 @@ TocEntry = TypedDict(
     "TocEntry", {"id": str, "label": str, "sub_headings": list[Subheading]}
 )
 
+HTMLAttrs: TypeAlias = list[tuple[str, str | None]]
+
+
+class TocParser(HTMLParser):
+    """
+    An HTML parser that looks for heading tags and builds a list of
+    TocEntries.
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.result: list[TocEntry] = []
+        self.last_seen_tag: str | None = None
+        self.last_seen_id: str | None = None
+        self.accumulator: list[str] = []
+
+    def handle_starttag(self, tag: str, attrs: HTMLAttrs) -> None:
+        """
+        If this is an opening heading tag, record the heading level and ID
+        and start a new state.
+        """
+        if tag in {"h2", "h3"}:
+            attr_dict = dict(attrs)
+            self.last_seen_tag = tag
+            self.last_seen_id = attr_dict["id"]
+            assert isinstance(self.last_seen_id, str)
+            self.accumulator = []
+
+    def handle_data(self, data: str) -> None:
+        """
+        If we're in the middle of a heading tag, add the data we've
+        received to the accumulator.
+        """
+        if self.last_seen_tag:
+            self.accumulator.append(data)
+
+    def handle_endtag(self, tag: str) -> None:
+        """
+        If this is a closing heading tag, process all the data we've
+        received until this point, append the result, then reset the state.
+        """
+        if tag == self.last_seen_tag:
+            label = "".join(self.accumulator).strip()
+            heading_id = self.last_seen_id
+            assert isinstance(heading_id, str)
+
+            if tag == "h2":
+                self.result.append(
+                    {"id": heading_id, "label": label, "sub_headings": []}
+                )
+            elif tag == "h3":
+                self.result[-1]["sub_headings"].append(
+                    {"id": heading_id, "label": label}
+                )
+            else:  # pragma: no cover
+                assert 0, "unreachable"
+
+            self.last_seen_tag = None
+            self.last_seen_id = None
+
 
 def parse_toc_entries(md: str) -> list[TocEntry]:
     """
     Extract all the headings from a Markdown document.
     """
     html = markdownify(md)
-    soup = BeautifulSoup(html, "html.parser")
-
-    result: list[TocEntry] = []
-
-    for heading_tag in soup.find_all(["h2", "h3"]):
-        heading_id = heading_tag.attrs["id"]
-        assert isinstance(heading_id, str)
-
-        if heading_tag.name == "h2":
-            result.append(
-                {
-                    "id": heading_id,
-                    "label": heading_tag.text,
-                    "sub_headings": [],
-                }
-            )
-        else:
-            result[-1]["sub_headings"].append(
-                {"id": heading_id, "label": heading_tag.text}
-            )
-
-    return result
+    parser = TocParser()
+    parser.feed(html)
+    return parser.result
--- tests/templates/test_table_of_contents.py
+++ tests/templates/test_table_of_contents.py
@@ -28,6 +28,7 @@ class TestTableOfContentsExtension:
             "{% table_of_contents %}\n"
             "\n"
             "## My first heading\n"
+            "Some text under the heading\n"
             "### Subheading 1\n"
             "### Subheading 2\n\n"
             "## My second heading"
@@ -45,6 +46,7 @@ class TestTableOfContentsExtension:
             "<li><a href=#my-second-heading>My second heading</a>"
             "</ul></nav> "
             "## My first heading "
+            "Some text under the heading "
             "### Subheading 1 "
             "### Subheading 2 "
             "## My second heading"
@@ -52,7 +54,7 @@ class TestTableOfContentsExtension:
 
     def test_render_toc_with_explicit_ids(self, env: Environment) -> None:
         """
-        Test the basic usage of the {% table_of_contents %} tag.
+        Test a table of contents where each heading has an explicit ID.
         """
         md = (
             "{% table_of_contents %}\n"
@@ -77,4 +79,35 @@ class TestTableOfContentsExtension:
             "<h3 id=subheading1a>Subheading 1</h3>"
             "<h3 id=subheading1b>Subheading 2</h3>"
             "<h2 id=heading2>My second heading</h2>"
+        )
+
+    def test_toc_with_inline_tags(self, env: Environment) -> None:
+        """
+        Test a table of contents where a heading includes markup.
+        """
+        md = (
+            "{% table_of_contents %}\n"
+            "\n"
+            "## My first heading\n"
+            "### This heading has `code` formatting\n"
+            "### This heading has *emphasis*\n"
+        )
+
+        html = env.from_string(md).render(page=StubPage(content=md)).strip()
+        assert minify_html.minify(html) == (
+            "<link href=css/components/table_of_contents.css rel=stylesheet>"
+            "<nav aria-labelledby=toc-heading class=table_of_contents>"
+            "<h3 id=toc-heading>Table of contents</h3>"
+            "<ul><li><a href=#my-first-heading>My first heading</a>"
+            "<ul>"
+            "<li><a href=#this-heading-has-code-code-code-formatting>"
+            # "This heading has <code>code</code> formatting</a></ul>"
+            "This heading has code formatting</a>"
+            "<li><a href=#this-heading-has-em-emphasis-em>"
+            # "This heading has <em>emphasis</em></a></ul>"
+            "This heading has emphasis</a></ul></ul>"
+            "</nav> "
+            "## My first heading "
+            "### This heading has `code` formatting "
+            "### This heading has *emphasis*"
         )
