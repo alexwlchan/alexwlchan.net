4447d290f2a66b1eb04f8eedf7e04a7389ec01c4
Alex Chan <alex@alexwlchan.net>
2026-08-17 19:43:04 +0100

    mosaic/css: cache the output of `parse_inline_styles`
    This was a hot path when rendering the site with a cold cache; parse
    the output and generally tidy up this file a bit.

--- mosaic/css.py
+++ mosaic/css.py
@@ -5,12 +5,12 @@ Code for dealing with CSS and website styles.
 from collections import OrderedDict
 from pathlib import Path
 import re
-from typing import TypedDict
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import lightningcss
+from pydantic import BaseModel
 
-from .cache import md5
+from .cache import get_cache, md5
 from .git import git_root
 
 
@@ -18,86 +18,115 @@ __all__ = ["CSS_DIR", "create_base_css", "get_inline_styles"]
 
 
 CSS_DIR = git_root() / "css"
-
-
-def create_base_css() -> tuple[str, str]:
-    """
-    Return the contents of the base CSS file for the site.
-
-    This resolves all @import rules into a single stylesheet.
-
-    Returns the filename and new CSS.
-    """
-    css_path = CSS_DIR / "style.css"
-
-    css = lightningcss.bundle_css(str(css_path), minify=True)  # type: ignore
-
-    # The lightningcss minifier collapses these text-decoration styles
-    # together, which looks wrong in WebKit. Undo this minification.
-    css = css.replace(
-        "text-decoration:underline 4px",
-        "text-decoration:underline;text-decoration-thickness:4px",
-    )
-
-    # Using three characters of hash gives me 16^3 = 4096 bits of entropy.
-    # Given I cache CSS for a year and only change it a handful of times,
-    # that should be plenty.
-    h = md5(css)[:3]
-
-    return f"style.{h}.css", css
-
-
-ParsedStyles = TypedDict("ParsedStyles", {"html": str, "styles": str})
-
+_CACHE = get_cache(".cache/css.db")
 
 STYLE_RE = re.compile(r"\s*<style[^>]*>(?P<css>.*?)</style>\s*", re.DOTALL)
 LINK_RE = re.compile(r"\s*(<link[^>]+>)\s*", re.DOTALL)
 EMPTY_DEFS_RE = re.compile(r"\s*<defs>\s*</defs>\s*")
 
 
+class ParsedStyles(BaseModel):
+    """
+    An HTML document free of inline <style> or <link> tags, and the
+    CSS that should be added to the document <head>.
+    """
+
+    html: str
+    styles: str = ""
+
+
+def create_base_css() -> tuple[str, str]:
+    """
+    Bundle and minify the base stylesheet into a single file string.
+
+    Resolves all `@import` statements in `css/style.css` and appends
+    a short hash for cache busting.
+
+    Returns a tuple of (hashed_filename, compiled_css).
+    """
+    css_path = CSS_DIR / "style.css"
+
+    css = lightningcss.bundle_css(str(css_path), minify=True)  # type: ignore
+
+    # Lightning CSS combines text-decoration properties, which renders
+    # incorrectly in WebKit. Restore separate text-decoration properties.
+    css = css.replace(
+        "text-decoration:underline 4px",
+        "text-decoration:underline;text-decoration-thickness:4px",
+    )
+
+    # 3 hex characters provide 16^3 = 4096 possible values.
+    # Given CSS is cached for a year and changes infrequently, the risk
+    # of a collision is negligible.
+    h = md5(css)[:3]
+
+    return f"style.{h}.css", css
+
+
 def get_inline_styles(html: str) -> ParsedStyles:
     """
-    Extract <style> tags from an HTML doc so they can be moved to the <head>.
+    Return parsed inline styles for an HTML document, caching the result.
+    """
+    cache_ns = "parse_inline_styles"
+    cache_key = md5(html)
 
-    I add inline <style> tags to my Markdown source files, but they're only
-    meant to go in the <head>. This file has a function that finds and extracts
-    all those inline tags, so they can be moved and de-duplicated.
+    if styles_json := _CACHE.get(cache_ns, cache_key):
+        return ParsedStyles.model_validate_json(styles_json)
+    else:
+        styles = parse_inline_styles(html)
+        _CACHE.set(cache_ns, cache_key, styles.model_dump_json())
+        return styles
+
+
+def parse_inline_styles(html: str) -> ParsedStyles:
+    """
+    Extract `<style>` tags and linked stylesheets from an HTML document.
+
+    This is used to extract inline styles added during Markdown rendering,
+    so they can be added to the `<head>` instead.
     """
     if "<style" not in html and "<link" not in html:
-        return {"html": html, "styles": ""}
+        return ParsedStyles(html=html)
 
-    # Contents of <style> tags we've discovered.
-    #
-    # This is an OrderedDict so duplicate styles will be removed.
+    # Deduplicate styles, but preserve the order they were found.
     # TODO(2026-01-21): Can lightningcss do this de-duplication for me?
     styles = OrderedDict[str, None]()
 
-    # Find and remove all <style> tags. I do this using regex because
-    # it's faster than parsing with BeautifulSoup, and avoids any
-    # changing of the behaviour or meaning of the HTML.
-    while m := STYLE_RE.search(html):
-        css = m.group("css")
-        styles[css] = None
-        html = html.replace(m.group(0), "")
+    # Extract and strip <style> tags.
+    #
+    # Use regex rather than BeautifulSoup because it's faster and avoids
+    # any unwanted changing of the HTML.
+    for m in STYLE_RE.finditer(html):
+        styles[m.group("css")] = None
 
-    # Find and remove all the <link> tags.
-    for m in LINK_RE.finditer(html):
-        link_tag = BeautifulSoup(m.group(0), "html.parser").find("link")
-        assert link_tag is not None
+    html = STYLE_RE.sub("", html)
 
-        if link_tag.attrs["rel"] != ["stylesheet"]:
-            continue
+    # Find, load, and remove external <link rel="stylesheet"> tags.
+    def _process_link_tag(match: re.Match[str]) -> str:
+        raw_tag = match.group(0)
+        link_tag = BeautifulSoup(raw_tag, "html.parser").find("link")
+        assert isinstance(link_tag, Tag), link_tag
 
-        href = link_tag.attrs["href"]
-        assert isinstance(href, str)
-        css = Path(href).read_text()
+        rel_attrs = link_tag.get("rel") or []
+        assert isinstance(rel_attrs, list), rel_attrs
 
-        styles[css] = None
-        html = html.replace(m.group(0), "")
+        if "stylesheet" not in rel_attrs:
+            return raw_tag
 
-    # If removing the <style> tags has rendered a set of <defs> empty,
-    # just remove them.
+        href = link_tag.get("href")
+        assert isinstance(href, str), href
+
+        file_path = CSS_DIR / Path(href).relative_to("css")
+        if not file_path.exists():
+            raise ValueError(f"<link> tag points to missing file: {href}")
+
+        styles[file_path.read_text()] = None
+        return ""
+
+    html = LINK_RE.sub(_process_link_tag, html)
+
+    # Clean up <defs> tags left empty after removing <style> tags.
     if "<defs" in html:
         html = EMPTY_DEFS_RE.sub("", html)
 
-    return {"html": html, "styles": "".join(styles)}
+    return ParsedStyles(html=html, styles="".join(styles))
--- tests/test_css.py
+++ tests/test_css.py
@@ -20,46 +20,54 @@ def test_css_has_no_imports() -> None:
 @pytest.mark.parametrize(
     "html, parsed_styles",
     [
-        ("<p>Hello world!</p>", {"html": "<p>Hello world!</p>", "styles": ""}),
-        (
+        pytest.param(
+            "<p>Hello world!</p>",
+            ParsedStyles(html="<p>Hello world!</p>", styles=""),
+            id="no_css",
+        ),
+        pytest.param(
             "<p>Hello world!</p>\n"
             "<style>p { color: red; }</style>\n"
             "<span>Greetings friends!</span>\n"
             "<style>span { color: blue; }</style>\n"
             "<style>p { color: red; }</style>\n",
-            {
-                "html": "<p>Hello world!</p><span>Greetings friends!</span>",
-                "styles": "p { color: red; }span { color: blue; }",
-            },
+            ParsedStyles(
+                html="<p>Hello world!</p><span>Greetings friends!</span>",
+                styles="p { color: red; }span { color: blue; }",
+            ),
+            id="inline_styles_with_duplicates",
         ),
-        (
+        pytest.param(
             "<p>Hello world!</p>\n<defs><style>p { color: red; }</style></defs>",
-            {
-                "html": "<p>Hello world!</p>",
-                "styles": "p { color: red; }",
-            },
+            ParsedStyles(
+                html="<p>Hello world!</p>",
+                styles="p { color: red; }",
+            ),
+            id="inline_styles_which_leave_empty_defs",
         ),
-        (
+        pytest.param(
             (
                 "<p>Hello world!</p>\n"
                 '<link href="css/components/dot_list.css" rel="stylesheet"/>'
             ),
-            {
-                "html": "<p>Hello world!</p>",
-                "styles": Path("css/components/dot_list.css").read_text(),
-            },
+            ParsedStyles(
+                html="<p>Hello world!</p>",
+                styles=Path("css/components/dot_list.css").read_text(),
+            ),
+            id="link_tag",
         ),
-        (
+        pytest.param(
             '<link as="image" href="/static/2025/truchet-header-light.svg" '
             'media="(prefers-color-scheme: light)" rel="preload" type="image/svg"/>',
-            {
-                "html": (
+            ParsedStyles(
+                html=(
                     '<link as="image" href="/static/2025/truchet-header-light.svg" '
                     'media="(prefers-color-scheme: light)" rel="preload" '
                     'type="image/svg"/>'
                 ),
-                "styles": "",
-            },
+                styles="",
+            ),
+            id="link_tag_non_stylesheet",
         ),
     ],
 )
@@ -68,3 +76,16 @@ def test_get_inline_styles(html: str, parsed_styles: ParsedStyles) -> None:
     Tests for get_inline_styles().
     """
     assert get_inline_styles(html) == parsed_styles
+
+
+def test_inline_style_with_invalid_link() -> None:
+    """
+    A <link> tag which points to a non-existent CSS file is an error.
+    """
+    html = (
+        "<p>Hello world!</p>\n"
+        '<link href="css/components/does_not_exist.css" rel="stylesheet"/>'
+    )
+
+    with pytest.raises(ValueError, match="missing file"):
+        get_inline_styles(html)
